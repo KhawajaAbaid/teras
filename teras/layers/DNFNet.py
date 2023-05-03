dac09ab88ce58afa7673540516c9bc77c4e08592
abaid <khawaja.abaid@gmail.com>
2023-05-03 14:00:20 +0500

    code cleaning and documentation

--- teras/layers/DNFNet.py
+++ teras/layers/DNFNet.py
@@ -9,33 +9,38 @@ from teras.utils.DNFNet import (compute_total_number_of_literals,
                                 create_conjunctions_indicator_matrix,
                                 create_formulas_indicator_matrix,
                                 dense_ndim_array_to_sparse_tensor,
-                                feature_selection,
                                 tf_matmul_sparse_dense,
                                 and_operator,
                                 or_operator,
-                                broadcast_exp,
                                 generate_random_mask,
                                 extension_matrix,
                                 binary_threshold)
 
 
-
 class FeatureSelection(keras.layers.Layer):
+    """
+    Feature Selection layer based on the Feature Selection component proposed by Liran Katzir et al.
+    in the paper Net-DNF: Effective Deep Modeling of Tabular Data.
+
+    Reference(s):
+        https://openreview.net/forum?id=73WTGs96kho
+
+    Args:
+        input_dim: Input dimensions of the input dataset
+        n_formulas: Number of DNF formulas. Each DNF formula is analogous to a tree in tree based ensembles.
+        elastic_net_beta: Use in the computation of Elastic Net Regularization. Defaults to 0.4
+        binary_threshold_eps: Used in the computation of learnable mask. Defaults to 1
+    """
     def __init__(self,
                  input_dim,
                  n_formulas=None,
-                 elastic_net_beta=None,
+                 elastic_net_beta=0.4,
                  binary_threshold_eps=1,
                  **kwagrs):
         super().__init__(**kwagrs)
         self.input_dim = input_dim
-        # self.keep_feature_prob_arr = keep_feature_prob_arr
-        # self.n_literals_per_formula_arr = n_literals_per_formula_arr
-        # self.learnable_mask = learnable_mask
         self.n_formulas = n_formulas
-        # self.elastic_net_alpha = elastic_net_alpha
         self.elastic_net_beta = elastic_net_beta
-        # self.extension_matrix_v = extension_matrix_v
         self.binary_threshold_eps = binary_threshold_eps
         self.v_temp = tf.Variable(tf.zeros(self.n_formulas), trainable=False)
 
@@ -77,13 +82,31 @@ class FeatureSelection(keras.layers.Layer):
 
 
 class Localization(keras.layers.Layer):
+    """
+    Localization layer based on the localization component proposed by Liran Katzir et al.
+    in the paper Net-DNF: Effective Deep Modeling of Tabular Data.
+
+    Reference(s):
+        https://openreview.net/forum?id=73WTGs96kho
+
+    Args:
+        input_dim: Input dimensions of the input dataset
+        n_formulas: Number of DNF formulas. Each DNF formula is analogous to a tree in tree based ensembles.
+        temperature: Temperature value. Defaults to 2.0.
+            According to the paper, The inclusion of an adaptive temperature in this localization mechanism
+            facilitates a data-dependent degree of exclusivity:
+            at high temperatures, only a few DNNFs will handle an input instance whereas
+            at low temperatures, more DNNFs will effectively participate in the ensemble.
+    """
     def __init__(self,
                  input_dim,
                  n_formulas,
+                 temperature=2.,
                  **kwargs):
         super().__init__(**kwargs)
         self.input_dim = input_dim
         self.n_formulas = n_formulas
+        self.temperature = temperature
         self.softmax = layers.Softmax()
 
     def build(self, input_shape):
@@ -94,13 +117,15 @@ class Localization(keras.layers.Layer):
         self.sigma = tf.Variable(initial_value=sigma_initializer(shape=(1, self.n_formulas, self.input_dim)),
                             shape=(1, self.n_formulas, self.input_dim), name="exp_sigma")
         self.temperature = tf.Variable(name='temperature',
-                                       initial_value=tf.constant(value=2.))
+                                       initial_value=tf.constant(value=self.temperature),
+                                       dtype=tf.float32)
 
     def call(self, inputs):
         diff = tf.expand_dims(inputs, axis=1) - tf.expand_dims(self.mu, axis=0)
         loc = tf.exp(-1 * tf.norm(tf.multiply(diff, self.sigma), axis=-1))
         loc = self.softmax(tf.sigmoid(self.temperature) * loc)
         return loc
+
 
 class DNNF(keras.layers.Layer):
     """
@@ -115,9 +140,9 @@ class DNNF(keras.layers.Layer):
         n_conjunctions_arr: Conjunctions array. If None, default values will be used.
         conjunctions_depth_arr: Conjunctions depth array. If None, default values will be used.
         keep_feature_prob_arr: Feature probability array. If None. default values will be used.
-        n_formulas: Number of formulas to use in DNNF layer
-        elastic_net_beta: Use in the creation of Elastic Net Reg. Defaults to 0.4
-        binary_threshold_eps: Used in the creation of learnable mask. Defaults to 1
+        n_formulas: Number of DNF formulas. Each DNF formula is analogous to a tree in tree based ensembles.
+        elastic_net_beta: Use in the computation of Elastic Net Regularization. Defaults to 0.4
+        binary_threshold_eps: Used in the computation of learnable mask. Defaults to 1
     """
     def __init__(self,
                  n_conjunctions_arr=None,
@@ -134,8 +159,6 @@ class DNNF(keras.layers.Layer):
         self.n_formulas = n_formulas
         self.elastic_net_beta = elastic_net_beta
         self.binary_threshold_eps = binary_threshold_eps
-        # self.softmax = layers.Softmax()
-
 
     def build(self, input_shape):
         self.input_dim = input_shape[-1]
@@ -147,44 +170,16 @@ class DNNF(keras.layers.Layer):
         self.localization = Localization(input_dim=self.input_dim,
                                          n_formulas=self.n_formulas)
 
-        # mu_initializer = initializers.random_normal()
-        # self.mu = tf.Variable(initial_value=mu_initializer(shape=(self.n_formulas, self.input_dim)),
-        #                  shape=(self.n_formulas, self.input_dim), name='exp_mu')
-        # sigma_initializer = initializers.random_normal()
-        # self.sigma = tf.Variable(initial_value=sigma_initializer(shape=(1, self.n_formulas, self.input_dim)),
-        #                     shape=(1, self.n_formulas, self.input_dim), name="exp_sigma")
-
-        # Learnable variables for feature selection
-        # self.elastic_net_alpha = tf.Variable(initial_value=tf.constant(0.),
-        #                                      name='elastic_net_alpha')
-        # self.learnable_mask = tf.Variable(initial_value=tf.fill(dims=(self.input_dim, self.n_formulas),
-        #                                                         value=self.binary_threshold_eps + 0.5),
-        #                                   shape=(self.input_dim, self.n_formulas),
-        #                                   name='learnable_mask')
-
         self.total_number_of_literals = compute_total_number_of_literals(self.n_formulas, self.n_conjunctions_arr,
                                                                     self.conjunctions_depth_arr)
         n_literals_per_formula_arr = compute_n_literals_per_formula(self.n_conjunctions_arr,
                                                                     self.conjunctions_depth_arr)
         total_number_of_conjunctions = compute_total_number_of_conjunctions(self.n_formulas, self.n_conjunctions_arr)
 
-        # extension_matrix_v is a prop to hold intermediate values.
-        # it isn't a matrix itself, but it's a build block for extension matrix
-        # The reason for using this variable is, because we can't assign values to a tensor.
-        # extension_matrix_v = tf.Variable(tf.zeros(self.n_formulas), trainable=False)
-        # self.learnable_binary_mask, self.literals_random_mask, self.elastic_net_reg = feature_selection(self.input_dim,
-        #                                                                                 self.keep_feature_prob_arr,
-        #                                                                                 n_literals_per_formula_arr,
-        #                                                                                 self.n_formulas,
-        #                                                                                 self.elastic_net_beta,
-        #                                                                                 self.learnable_mask,
-        #                                                                                 self.elastic_net_alpha,
-        #                                                                                 extension_matrix_v)
         self.learnable_binary_mask, self.literals_random_mask, self.elastic_net_reg = self.feature_selection(
                                                                                         self.keep_feature_prob_arr,
                                                                                         n_literals_per_formula_arr)
 
-        # tf.shape(self.literals_random_mask)[1] == total_number_of_literals
         self.bias = tf.Variable(name='literals_bias',
                                 shape=tf.shape(self.literals_random_mask)[1],
                                 initial_value=tf.fill(dims=(tf.shape(self.literals_random_mask)[1],), value=0.))
@@ -192,8 +187,7 @@ class DNNF(keras.layers.Layer):
                                       shape=tf.shape(self.literals_random_mask),
                                       initializer=initializers.glorot_normal())
         self.weight = tf.multiply(self.weight, self.literals_random_mask)
-        # self.temperature = tf.Variable(name='temperature',
-        #                                initial_value=tf.constant(value=2.))
+
         # 'b' is just a prop to hold intermediate values since we can't assign values to a tensor,
         # so we define a variable here with trainable argument set to False.
         # same is the case with 'c' below.
@@ -219,13 +213,13 @@ class DNNF(keras.layers.Layer):
         x = inputs
         # Creating affine literals
         out_literals = tf.tanh(tf.add(tf.matmul(x, tf.multiply(self.weight, self.learnable_binary_mask)), self.bias))
+        # Soft Conjunctions
         out_conjunctions = and_operator(tf_matmul_sparse_dense(self.conjunctions_indicator_sparse_matrix, out_literals),
                                         d=self.and_bias)
         out_DNNFs = or_operator(tf_matmul_sparse_dense(self.formulas_indicator_sparse_matrix, out_conjunctions),
                                 d=self.or_bias)
         out_DNNFs = tf.reshape(out_DNNFs, shape=(-1, self.n_formulas))
-        # loc = broadcast_exp(x, mu=self.mu, sigma=self.sigma)
-        # loc = self.softmax(tf.sigmoid(self.temperature) * loc)
+        # Localization
         loc = self.localization(x)
         out_DNNFs = tf.multiply(out_DNNFs, loc)
         return out_DNNFs
