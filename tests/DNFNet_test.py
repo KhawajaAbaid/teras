c712e3b1f500977cae57b6d04d372aae4102977f
abaid <khawaja.abaid@gmail.com>
2023-05-06 16:12:33 +0500

    code cleaning

--- tests/DNFNet_test.py
+++ tests/DNFNet_test.py
@@ -1,23 +1,28 @@
 from tensorflow import keras
 from teras.models import DNFNetRegressor, DNFNetClassifier
 from tensorflow.keras.datasets import boston_housing
-# import tensorflow as tf
-
-# (train_data, train_targets), (test_data, test_target) = (boston_housing.load_data())
+import tensorflow as tf
 
 
+# PLEASE NOTE: The training does NOT get stuck, it just takes a while to start
+# because of the complex tensorflow graph building. But once it starts, it literally zooms.
+
+# <<<<<<<<<<<<<<<<<<<<< REGRESSION TEST >>>>>>>>>>>>>>>>>>>>>>>>>>>
+print(F"{'-'*15} REGRESSSION TEST {'-'*15}")
+(train_data, train_targets), (test_data, test_target) = (boston_housing.load_data())
 # train_targets = tf.cast(train_targets, dtype="float32")
 # test_target = tf.cast(test_target, dtype="float32")
 
-# dnf_model = DNFNetRegressor(n_formulas=256)
-#
-# dnf_model.compile(loss=keras.losses.MSE, metrics=keras.metrics.MAE,
-#                   optimizer=keras.optimizers.Adam(learning_rate=0.05))
-#
-# dnf_model.fit(train_data, train_targets, batch_size=32, epochs=10)
+dnf_model = DNFNetRegressor(n_formulas=256)
+
+dnf_model.compile(loss=keras.losses.MSE, metrics=keras.metrics.MAE,
+                  optimizer=keras.optimizers.Adam(learning_rate=0.05))
+
+dnf_model.fit(train_data, train_targets, batch_size=32, epochs=10)
 
 
-# Classification test
+# <<<<<<<<<<<<<<<<<<<<< CLASSIFICATION TEST >>>>>>>>>>>>>>>>>>>>>>>>>>>
+print(F"{'-'*15} CLASSIFICAITON TEST {'-'*15}")
 from sklearn import datasets as sklearn_datasets
 
 iris_dataset = sklearn_datasets.load_iris()
