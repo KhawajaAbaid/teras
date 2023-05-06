63b2f4a8d10e2944ff8cd1a6fc441449efcb62cd
abaid <khawaja.abaid@gmail.com>
2023-05-06 12:49:07 +0500

    code cleaning and correction

--- tests/NODE_test.py
+++ tests/NODE_test.py
@@ -1,6 +1,6 @@
 import keras.optimizers
 
-from teras.models import NODEClassifier
+from teras.models import NODEClassifier, NODERegressor
 from tensorflow.keras.datasets import boston_housing
 import tensorflow as tf
 from sklearn import datasets as sklearn_datasets
@@ -8,8 +8,8 @@ from sklearn import datasets as sklearn_datasets
 (train_data, train_targets), (test_data, test_target) = (boston_housing.load_data())
 
 # Regression Test
-# node_regressor = NODE(n_trees=16,
-#                         n_layers=8)
+# node_regressor = NODERegressor(n_trees=16,
+#                                 n_layers=8)
 #
 # node_regressor.compile(loss="MSE", optimizer=keras.optimizers.Adam(learning_rate=0.05), metrics=["MAE"])
 # node_regressor.fit(train_data, train_targets, batch_size=32, epochs=10)
