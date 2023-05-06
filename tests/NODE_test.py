5e15403495600dfa8717ea06c1877eb483f78db4
abaid <khawaja.abaid@gmail.com>
2023-05-06 16:15:53 +0500

    code cleaning

--- tests/NODE_test.py
+++ tests/NODE_test.py
@@ -1,21 +1,24 @@
-import keras.optimizers
-
+from tensorflow import keras
 from teras.models import NODEClassifier, NODERegressor
 from tensorflow.keras.datasets import boston_housing
 import tensorflow as tf
 from sklearn import datasets as sklearn_datasets
 
+
+# <<<<<<<<<<<<<<<<<<< REGRESSION Test >>>>>>>>>>>>>>>>>>>>>>>>>>
 (train_data, train_targets), (test_data, test_target) = (boston_housing.load_data())
+print(f"{'-'*15} REGRESSION TEST {'-'*15}")
+node_regressor = NODERegressor(n_trees=16,
+                                n_layers=8)
 
-# Regression Test
-# node_regressor = NODERegressor(n_trees=16,
-#                                 n_layers=8)
-#
-# node_regressor.compile(loss="MSE", optimizer=keras.optimizers.Adam(learning_rate=0.05), metrics=["MAE"])
-# node_regressor.fit(train_data, train_targets, batch_size=32, epochs=10)
-#
+node_regressor.compile(loss="MSE", optimizer=keras.optimizers.Adam(learning_rate=0.05), metrics=["MAE"])
+node_regressor.fit(train_data, train_targets, batch_size=32, epochs=10)
 
-# Classification Test
+
+
+
+# <<<<<<<<<<<<<<<<<<< CLASSFICIATION Test >>>>>>>>>>>>>>>>>>>>>>>>>>
+print(f"{'-'*15} CLASSFICIATION TEST {'-'*15}")
 iris_dataset = sklearn_datasets.load_iris()
 X = iris_dataset.data
 y = iris_dataset.target
