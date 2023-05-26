dec1e0a318be062ed495d07ba865bba8ac9b39a7
abaid <khawaja.abaid@gmail.com>
2023-05-26 11:58:41 +0500

    add CTGAN, CTGANGenerator and CTGANDiscriminator imports to data_generators __init__

--- teras/data_generators/__init__.py
+++ teras/data_generators/__init__.py
@@ -11,3 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.5
+
+
+# CTGAN
+from teras.models.CTGAN import (CTGAN,
+                                Generator as CTGANGenerator,
+                                Discriminator as CTGANDiscriminator,
+                                )
