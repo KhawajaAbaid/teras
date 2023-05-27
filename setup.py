17aa8b54bbc896c829b175b8b9d285c4cc4a8d39
abaid <khawaja.abaid@gmail.com>
2023-05-27 15:16:48 +0500

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='teras',
+    version='0.1.0',
+    description='A Unified Deep Learning Framework for Tabular Data.',
+    author='Khawaja Abaid',
+    author_email='khawaja.abaid@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow',
+        'tensorflow-addons',
+        'tensorflow-probability',
+        'pandas',
+        'scikit-learn'
+        'numpy',
+    ],
+)
