05493c9780aae1fd49bfde361328a375b8b4566b
abaid <khawaja.abaid@gmail.com>
2023-06-09 16:55:24 +0500

    Update docstrings, variables and parameters

--- teras/models/base/gan.py
+++ teras/models/base/gan.py
@@ -18,13 +18,19 @@ class BaseGenerator(keras.Model):
         units_hidden: `list` | `tuple`, A list or tuple of
             units which determines the number and hidden
             dimensionality of the building blocks to be created.
-        hidden_layer: Either `layers.Dense` class
-            or a `subclass of layers.Dense`
-            You must pass the class and NOT instance of the class.
-            Used to construct the hidden block in the Generator.
-            If units_hidden are passed but hidden_layer is None,
+        hidden_layer: `keras.layers.Layer`, any subclass of `Layer`
+            class, such as `Dense` or customized layer such as a
+            `ResidualBlock` etc.
+            REMEMBER, you must pass the class and NOT instance of the class.
+            It is used to construct the hidden block in the Generator.
+            If `units_hidden` is passed but `hidden_layer` is None,
             then a `Dense` layer with `relu` activation is used.
-        units_out: int, Number of units to use in output dense layer,
+        output_layer: `keras.layers.Layer`, any subclass of `Layer` class,
+            such as `Dense` or a customized layer.
+            You must pass the class and NOT instance of the class.
+            If `units_out` is passed but `output_layer` is `None`,
+            then a `Dense` layer with no activation is used.
+        units_out: int, Number of units to use in output layer,
             or the dimensionality of the output.
             If None, no separate output dense layer will be added,
             and the output will have the dimensions of last hidden layer.
@@ -32,14 +38,14 @@ class BaseGenerator(keras.Model):
     def __init__(self,
                  units_hidden: LIST_OR_TUPLE = None,
                  hidden_layer=None,
-                 units_out: int = None,
                  output_layer=None,
+                 units_out: int = None,
                  **kwargs):
         super().__init__(**kwargs)
         self.units_hidden = units_hidden
         self.hidden_layer = hidden_layer
-        self.units_out = units_out
         self.output_layer = output_layer
+        self.units_out = units_out
 
         self.hidden_block = None
         self.dense_out = None
@@ -53,24 +59,24 @@ class BaseGenerator(keras.Model):
                     self.hidden_block.add(self.hidden_layer(units))
         if self.units_out is not None:
             if self.output_layer is None:
-                self.dense_out = layers.Dense(self.units_out)
+                self.out = layers.Dense(self.units_out)
             else:
-                self.dense_out = self.output_layer(self.units_out)
+                self.out = self.output_layer(self.units_out)
 
     def call(self, inputs):
         x = inputs
         if self.hidden_block is not None:
             x = self.hidden_block(x)
-        if self.dense_out is not None:
-            x = self.dense_out(x)
+        if self.out is not None:
+            x = self.out(x)
         return x
 
     def get_config(self):
         config = super().get_config()
         config.update({'units_hidden': self.hidden_dims,
                        'hidden_layer': self.hidden_layer,
-                       'units_out': self.units_out,
                        'output_layer': self.output_layer,
+                       'units_out': self.units_out,
                        })
         return config
 
@@ -98,7 +104,7 @@ class BaseDiscriminator(keras.Model):
             You must pass the class and NOT instance of the class.
             If `units_out` is passed but `output_layer` is `None`,
             then a `Dense` layer with no activation is used.
-        units_out: int, Number of units to use in output dense layer,
+        units_out: int, Number of units to use in output layer,
             or the dimensionality of the output.
             If None, no separate output dense layer will be added,
             and the output will have the dimensions of last hidden layer.
@@ -144,8 +150,8 @@ class BaseDiscriminator(keras.Model):
         config = super().get_config()
         config.update({'units_hidden': self.hidden_dims,
                        'hidden_layer': self.hidden_layer,
-                       'units_out': self.units_out,
                        'output_layer': self.output_layer,
+                       'units_out': self.units_out,
                        })
         return config
 
