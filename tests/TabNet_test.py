2493b7d15ee2a4e33ef30f56e84d15d6f9280018
abaid <khawaja.abaid@gmail.com>
2023-06-15 12:02:00 +0500

    Update tabnet test

--- tests/TabNet_test.py
+++ tests/TabNet_test.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 from tensorflow.keras.datasets import boston_housing
 import tensorflow as tf
 from sklearn import datasets as sklearn_datasets
+tf.config.run_functions_eagerly(True)
 
 (train_data, train_targets), (test_data, test_target) = (boston_housing.load_data())
 
@@ -24,14 +25,8 @@ iris_dataset = sklearn_datasets.load_iris()
 X = iris_dataset.data
 y = iris_dataset.target
 
-tabnet_classifier = TabNetClassifier(units=64,
-                                     num_classes=3,
-                                     decision_step_output_dim=64,
-                                     num_decision_steps=5,
-                                     relaxation_factor=1.5,
-                                     batch_momentum=0.98,
-                                     virtual_batch_size=None,
-                                     epsilon=1e-5)
+tabnet_classifier = TabNetClassifier(num_classes=3,
+                                     virtual_batch_size=2)
 tabnet_classifier.compile(loss=keras.losses.SparseCategoricalCrossentropy(), optimizer="Adam",
                           metrics=[keras.metrics.SparseCategoricalAccuracy()])
 tabnet_classifier.fit(X, y, batch_size=32, epochs=10)
