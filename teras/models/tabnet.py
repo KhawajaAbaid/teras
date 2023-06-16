863c9992bd004762513bda2b5319e31dab2e3689
abaid <khawaja.abaid@gmail.com>
2023-06-16 12:01:46 +0500

    Update docstrings and parameters

--- teras/models/tabnet.py
+++ teras/models/tabnet.py
@@ -16,41 +16,80 @@ class TabNet(keras.Model):
         https://arxiv.org/abs/1908.07442
 
     Args:
-        feature_transformer_dim: Number of hidden units to use in Fully Connected (Dense) layer of Feature Transformer
-        decision_step_output_dim: Output dimensionality for the decision step
-        num_decision_steps: Number of decision steps.
-                            According to the paper, "TabNet uses sequential attention to choose which features
-                            to reason from at each decision step"
-        relaxation_factor: When = 1, a feature is enforced to be used only at one decision step
-                        and as it increases, more flexibility is provided to use a feature at multiple decision steps.
-        batch_momentum: Momentum value to use for BatchNormalization layer
-        virtual_batch_size: Batch size to use for virtual_batch_size parameter in BatchNormalization layer
-        epsilon: Epsilon is a small number for numerical stability.
+        feature_transformer_dim: `int`, default 32, the dimensionality of the hidden
+            representation in feature transformation block.
+            Each layer first maps the representation to a `2 * feature_transformer_dim`
+            output and half of it is used to determine the
+            non-linearity of the GLU activation where the other half is used as an
+            input to GLU, and eventually `feature_transformer_dim` output is
+            transferred to the next layer.
+        decision_step_output_dim: `int`, default 32, the dimensionality of output at each
+            decision step, which is later mapped to the final classification or regression output.
+            It is recommended to keep `decision_step_output_dim` and `feature_transformer_dim`
+            equal to each other.
+            Adjusting these two parameters values is a good way of obtaining a tradeoff between
+            performance and complexity.
+        num_decision_steps: `int`, default 5, the number of sequential decision steps.
+            For most datasets a value in the range [3, 10] is optimal.
+            If there are more informative features in the dataset, the value tends to
+            be higher. That said, a very high value of `num_decision_steps` may suffer
+            from overfitting.
+        num_shared_layers: `int`, default 2. Number of shared layers to use in the Feature Transformer.
+            These shared layers are `shared` across decision steps.
+        num_decision_dependent_layers: `int`, default 2. Number of decision dependent layers to use in
+            the Feature Transformer. In simple words, `num_decision_dependent_layers` are created
+            for each decision step in the `num_decision_steps`.
+            For instance, if `num_decision_steps = 5` and  `num_decision_dependent_layers = 2`
+            then 10 layers will be created, 2 for each decision step.
+        relaxation_factor: `float`, default 1.5, Relaxation factor that promotes the reuse of each
+            feature at different decision steps. When it is 1, a feature is enforced
+            to be used only at one decision step and as it increases, more
+            flexibility is provided to use a feature at multiple decision steps.
+            An optimal value of relaxation_factor can have a major role on the performance.
+            Typically, a larger value for `num_decision_steps` favors for a larger `relaxation_factor`.
+        batch_momentum: `float`, default 0.9, Momentum value to use for BatchNormalization layer.
+        virtual_batch_size: `int`, default 64, Batch size to use for `virtual_batch_size`
+            parameter in BatchNormalization layer.
+            This is typically much smaller than the `batch_size` used for training.
+        residual_normalization_factor: `float`, default 0.5, In the feature transformer, except for the
+            layer, every other layer utilizes normalized residuals, where `residual_normalization_factor`
+            determines the scale of normalization.
+        epsilon: `float`, default 0.00001, Epsilon is a small number for numerical stability
+            during the computation of entropy loss.
     """
     def __init__(self,
                  feature_transformer_dim: int = 32,
                  decision_step_output_dim: int = 32,
                  num_decision_steps: int = 5,
-                 relaxation_factor=1.5,
-                 batch_momentum=0.7,
-                 virtual_batch_size: int = 16,
+                 num_shared_layers: int = 2,
+                 num_decision_dependent_layers: int = 2,
+                 relaxation_factor: float = 1.5,
+                 batch_momentum: float = 0.9,
+                 virtual_batch_size: int = 64,
+                 residual_normalization_factor: float = 0.5,
                  epsilon=1e-5,
                  **kwargs):
         super().__init__(**kwargs)
         self.feature_transformer_dim = feature_transformer_dim
         self.decision_step_output_dim = decision_step_output_dim
         self.num_decision_steps = num_decision_steps
+        self.num_shared_layers = num_shared_layers
+        self.num_decision_dependent_layers = num_decision_dependent_layers
         self.relaxation_factor = relaxation_factor
         self.batch_momentum = batch_momentum
         self.virtual_batch_size = virtual_batch_size
+        self.residual_normalization_factor = residual_normalization_factor
         self.epsilon = epsilon
 
         self.encoder = TabNetEncoder(feature_transformer_dim=self.feature_transformer_dim,
                                      decision_step_output_dim=self.decision_step_output_dim,
                                      num_decision_steps=self.num_decision_steps,
+                                     num_shared_layers=self.num_shared_layers,
+                                     num_decision_dependent_layers=self.num_decision_dependent_layers,
                                      relaxation_factor=self.relaxation_factor,
                                      batch_momentum=self.batch_momentum,
                                      virtual_batch_size=self.virtual_batch_size,
+                                     residual_normalization_factor=self.residual_normalization_factor,
                                      epsilon=self.epsilon)
 
     def call(self, inputs):
@@ -66,36 +105,70 @@ class TabNetClassifier(TabNet):
         https://arxiv.org/abs/1908.07442
 
     Args:
-        num_classes: Number of classes to predict
-        feature_transformer_dim: Number of hidden units to use in Fully Connected (Dense) layer
-        decision_step_output_dim: Output dimensionality for the decision step
-        num_decision_steps: Number of decision steps.
-                            According to the paper, "TabNet uses sequential attention to choose which features
-                            to reason from at each decision step"
-        relaxation_factor: When = 1, a feature is enforced to be used only at one decision step
-                        and as it increases, more flexibility is provided to use a feature at multiple decision steps.
-        batch_momentum: Momentum value to use for BatchNormalization layer
-        virtual_batch_size: Batch size to use for virtual_batch_size parameter in BatchNormalization layer
-        epsilon: Epsilon is a small number for numerical stability.
-        output_activation: Activation layer to use for classification output.
-                        By default, Sigmoid is used for binary while Softmax is used for Multiclass classification.
+        num_classes: Number of classes to predict.
+        feature_transformer_dim: `int`, default 32, the dimensionality of the hidden
+            representation in feature transformation block.
+            Each layer first maps the representation to a `2 * feature_transformer_dim`
+            output and half of it is used to determine the
+            non-linearity of the GLU activation where the other half is used as an
+            input to GLU, and eventually `feature_transformer_dim` output is
+            transferred to the next layer.
+        decision_step_output_dim: `int`, default 32, the dimensionality of output at each
+            decision step, which is later mapped to the final classification or regression output.
+            It is recommended to keep `decision_step_output_dim` and `feature_transformer_dim`
+            equal to each other.
+            Adjusting these two parameters values is a good way of obtaining a tradeoff between
+            performance and complexity.
+        num_decision_steps: `int`, default 5, the number of sequential decision steps.
+            For most datasets a value in the range [3, 10] is optimal.
+            If there are more informative features in the dataset, the value tends to
+            be higher. That said, a very high value of `num_decision_steps` may suffer
+            from overfitting.
+        num_shared_layers: `int`, default 2. Number of shared layers to use in the Feature Transformer.
+            These shared layers are `shared` across decision steps.
+        num_decision_dependent_layers: `int`, default 2. Number of decision dependent layers to use in
+            the Feature Transformer. In simple words, `num_decision_dependent_layers` are created
+            for each decision step in the `num_decision_steps`.
+            For instance, if `num_decision_steps = 5` and  `num_decision_dependent_layers = 2`
+            then 10 layers will be created, 2 for each decision step.
+        relaxation_factor: `float`, default 1.5, Relaxation factor that promotes the reuse of each
+            feature at different decision steps. When it is 1, a feature is enforced
+            to be used only at one decision step and as it increases, more
+            flexibility is provided to use a feature at multiple decision steps.
+            An optimal value of relaxation_factor can have a major role on the performance.
+            Typically, a larger value for `num_decision_steps` favors for a larger `relaxation_factor`.
+        batch_momentum: `float`, default 0.9, Momentum value to use for BatchNormalization layer.
+        virtual_batch_size: `int`, default 64, Batch size to use for `virtual_batch_size`
+            parameter in BatchNormalization layer.
+            This is typically much smaller than the `batch_size` used for training.
+        residual_normalization_factor: `float`, default 0.5, In the feature transformer, except for the
+            layer, every other layer utilizes normalized residuals, where `residual_normalization_factor`
+            determines the scale of normalization.
+        epsilon: `float`, default 0.00001, Epsilon is a small number for numerical stability
+            during the computation of entropy loss.
     """
     def __init__(self,
                  num_classes=2,
-                 feature_transformer_dim=32,
+                 feature_transformer_dim: int = 32,
                  decision_step_output_dim: int = 32,
                  num_decision_steps: int = 5,
-                 relaxation_factor=1.5,
-                 batch_momentum=0.7,
-                 virtual_batch_size: int = 16,
+                 num_shared_layers: int = 2,
+                 num_decision_dependent_layers: int = 2,
+                 relaxation_factor: float = 1.5,
+                 batch_momentum: float = 0.9,
+                 virtual_batch_size: int = 64,
+                 residual_normalization_factor: float = 0.5,
                  epsilon=1e-5,
                  **kwargs):
         super().__init__(feature_transformer_dim=feature_transformer_dim,
                          decision_step_output_dim=decision_step_output_dim,
                          num_decision_steps=num_decision_steps,
+                         num_shared_layers=num_shared_layers,
+                         num_decision_dependent_layers=num_decision_dependent_layers,
                          relaxation_factor=relaxation_factor,
                          batch_momentum=batch_momentum,
                          virtual_batch_size=virtual_batch_size,
+                         residual_normalization_factor=residual_normalization_factor,
                          epsilon=epsilon,
                          **kwargs)
         self.num_classes = 1 if num_classes <= 2 else num_classes
@@ -118,33 +191,69 @@ class TabNetRegressor(TabNet):
 
     Args:
         num_outputs: Number of regression outputs.
-        feature_transformer_dim: Number of hidden units to use in Fully Connected (Dense) layer in FeatureTransformer
-        decision_step_output_dim: Output dimensionality for the decision step
-        num_decision_steps: Number of decision steps.
-                            According to the paper, "TabNet uses sequential attention to choose which features
-                            to reason from at each decision step"
-        relaxation_factor: When = 1, a feature is enforced to be used only at one decision step
-                        and as it increases, more flexibility is provided to use a feature at multiple decision steps.
-        batch_momentum: Momentum value to use for BatchNormalization layer
-        virtual_batch_size: Batch size to use for virtual_batch_size parameter in BatchNormalization layer
-        epsilon: Epsilon is a small number for numerical stability.
+        feature_transformer_dim: `int`, default 32, the dimensionality of the hidden
+            representation in feature transformation block.
+            Each layer first maps the representation to a `2 * feature_transformer_dim`
+            output and half of it is used to determine the
+            non-linearity of the GLU activation where the other half is used as an
+            input to GLU, and eventually `feature_transformer_dim` output is
+            transferred to the next layer.
+        decision_step_output_dim: `int`, default 32, the dimensionality of output at each
+            decision step, which is later mapped to the final classification or regression output.
+            It is recommended to keep `decision_step_output_dim` and `feature_transformer_dim`
+            equal to each other.
+            Adjusting these two parameters values is a good way of obtaining a tradeoff between
+            performance and complexity.
+        num_decision_steps: `int`, default 5, the number of sequential decision steps.
+            For most datasets a value in the range [3, 10] is optimal.
+            If there are more informative features in the dataset, the value tends to
+            be higher. That said, a very high value of `num_decision_steps` may suffer
+            from overfitting.
+        num_shared_layers: `int`, default 2. Number of shared layers to use in the Feature Transformer.
+            These shared layers are `shared` across decision steps.
+        num_decision_dependent_layers: `int`, default 2. Number of decision dependent layers to use in
+            the Feature Transformer. In simple words, `num_decision_dependent_layers` are created
+            for each decision step in the `num_decision_steps`.
+            For instance, if `num_decision_steps = 5` and  `num_decision_dependent_layers = 2`
+            then 10 layers will be created, 2 for each decision step.
+        relaxation_factor: `float`, default 1.5, Relaxation factor that promotes the reuse of each
+            feature at different decision steps. When it is 1, a feature is enforced
+            to be used only at one decision step and as it increases, more
+            flexibility is provided to use a feature at multiple decision steps.
+            An optimal value of relaxation_factor can have a major role on the performance.
+            Typically, a larger value for `num_decision_steps` favors for a larger `relaxation_factor`.
+        batch_momentum: `float`, default 0.9, Momentum value to use for BatchNormalization layer.
+        virtual_batch_size: `int`, default 64, Batch size to use for `virtual_batch_size`
+            parameter in BatchNormalization layer.
+            This is typically much smaller than the `batch_size` used for training.
+        residual_normalization_factor: `float`, default 0.5, In the feature transformer, except for the
+            layer, every other layer utilizes normalized residuals, where `residual_normalization_factor`
+            determines the scale of normalization.
+        epsilon: `float`, default 0.00001, Epsilon is a small number for numerical stability
+            during the computation of entropy loss.
     """
     def __init__(self,
-                 num_outputs: int = 1,
+                 num_outputs=1,
                  feature_transformer_dim: int = 32,
                  decision_step_output_dim: int = 32,
                  num_decision_steps: int = 5,
-                 relaxation_factor=1.5,
-                 batch_momentum=0.7,
-                 virtual_batch_size: int = 16,
+                 num_shared_layers: int = 2,
+                 num_decision_dependent_layers: int = 2,
+                 relaxation_factor: float = 1.5,
+                 batch_momentum: float = 0.9,
+                 virtual_batch_size: int = 64,
+                 residual_normalization_factor: float = 0.5,
                  epsilon=1e-5,
                  **kwargs):
         super().__init__(feature_transformer_dim=feature_transformer_dim,
                          decision_step_output_dim=decision_step_output_dim,
                          num_decision_steps=num_decision_steps,
+                         num_shared_layers=num_shared_layers,
+                         num_decision_dependent_layers=num_decision_dependent_layers,
                          relaxation_factor=relaxation_factor,
                          batch_momentum=batch_momentum,
                          virtual_batch_size=virtual_batch_size,
+                         residual_normalization_factor=residual_normalization_factor,
                          epsilon=epsilon,
                          **kwargs)
         self.num_outputs = num_outputs
