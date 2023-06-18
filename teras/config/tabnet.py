1be6dc71310c93159bf83808445559510f9d39e2
abaid <khawaja.abaid@gmail.com>
2023-06-18 13:37:11 +0500

    Remove unnecessary classes

--- teras/config/tabnet.py
+++ teras/config/tabnet.py
@@ -13,35 +13,3 @@ class TabNetConfig(BaseConfig):
     virtual_batch_size: int = 64
     residual_normalization_factor: float = 0.5
     epsilon = 1e-5
-
-
-class TabNetRegressorConfig(TabNetConfig):
-    num_outputs: int = 1
-
-
-class TabNetClassifierConfig(TabNetConfig):
-    num_classes: int = 2
-
-
-class TabNetPreTrainerConfig(BaseConfig):
-    data_dim: int = 1
-    missing_feature_probability: float = 0.3
-    categorical_features_vocabulary: dict = None
-    relaxation_factor: float = 1.5
-    batch_momentum: float = 0.9
-    virtual_batch_size: int = 64
-    residual_normalization_factor: float = 0.5
-    epsilon = 1e-5
-    # Encoder specific paramters
-    encoder_feature_transformer_dim: int = 32
-    encoder_decision_step_output_dim: int = 32
-    encoder_num_decision_steps: int = 5
-    encoder_num_shared_layers: int = 2
-    encoder_num_decision_dependent_layers: int = 2
-    # Decoder specific parameters
-    decoder_feature_transformer_dim: int = 32
-    decoder_decision_step_output_dim: int = 32
-    decoder_num_decision_steps: int = 5
-    decoder_num_shared_layers: int = 2
-    decoder_num_decision_dependent_layers: int = 2
-
