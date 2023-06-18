972ba85e5abb9445d5b45ed09df4fc24f1de24a4
abaid <khawaja.abaid@gmail.com>
2023-06-18 13:39:33 +0500

    Implement FitConfig class to allow user to configure fit params when fit isn't directly available

--- teras/config/base.py
+++ teras/config/base.py
@@ -17,3 +17,28 @@ class BaseConfig:
                 current_class = current_class.__base__
         return attrs
 
+
+class FitConfig(BaseConfig):
+    """
+    Config class for model's fit method.
+    It is to be used when a function wraps a model and calls model's fit method
+    such that the model's `fit` method isn't exposed to user directly and
+    hence user cannot specify the parameters.
+    """
+    batch_size = None
+    epochs = 1
+    verbose = 'auto'
+    callbacks = None
+    validation_split = 0.0
+    validation_data = None
+    shuffle = True
+    class_weight = None
+    sample_weight = None
+    initial_epoch = 0
+    steps_per_epoch = None
+    validation_steps = None
+    validation_batch_size = None
+    validation_freq = 1
+    max_queue_size = 10
+    workers = 1
+    use_multiprocessing = False
