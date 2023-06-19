49fe2eb403f5151aae8c6b0a4321453d00db0fe8
abaid <khawaja.abaid@gmail.com>
2023-06-19 16:54:30 +0500

    Make FitConfig attributes instance attributes instead of class attributes

--- teras/config/base.py
+++ teras/config/base.py
@@ -25,20 +25,22 @@ class FitConfig(BaseConfig):
     such that the model's `fit` method isn't exposed to user directly and
     hence user cannot specify the parameters.
     """
-    batch_size = None
-    epochs = 1
-    verbose = 'auto'
-    callbacks = None
-    validation_split = 0.0
-    validation_data = None
-    shuffle = True
-    class_weight = None
-    sample_weight = None
-    initial_epoch = 0
-    steps_per_epoch = None
-    validation_steps = None
-    validation_batch_size = None
-    validation_freq = 1
-    max_queue_size = 10
-    workers = 1
-    use_multiprocessing = False
+    def __init__(self):
+        super().__init__()
+        batch_size = None
+        epochs = 1
+        verbose = 'auto'
+        callbacks = None
+        validation_split = 0.0
+        validation_data = None
+        shuffle = True
+        class_weight = None
+        sample_weight = None
+        initial_epoch = 0
+        steps_per_epoch = None
+        validation_steps = None
+        validation_batch_size = None
+        validation_freq = 1
+        max_queue_size = 10
+        workers = 1
+        use_multiprocessing = False
