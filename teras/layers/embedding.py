67e7301f87d5a6ef67214f40a38ce32e61166dc2
abaid <khawaja.abaid@gmail.com>
2023-06-19 15:11:26 +0500

    Update CategoricalFeaturesEmbedding to handle dataset of both types i.e. Dictionary and Array

--- teras/layers/embedding.py
+++ teras/layers/embedding.py
@@ -14,88 +14,96 @@ class CategoricalFeaturesEmbedding(layers.Layer):
 
     Args:
         categorical_features_vocabulary: `dict`, Vocabulary of categorical feature.
-            Vocabulary is simply a dictionary where feature name serves as key and its value
-            is the sorted list of all unique values in that feature.
+            Vocabulary is simply a dictionary where feature name maps
+            to a tuple of feature index and a list of unique values in the feature.
             You can get this vocabulary by calling
-            # TODO rename the function to get_categorical_features_vocabulary
-            `teras.utils.get_categorical_features_vocab(dataset, categorical_features)`
+            `teras.utils.get_categorical_features_vocabulary(dataset, categorical_features)`
         embedding_dim: `int`, default 32, Dimensionality of the embeddings
             to which categorical features should be mapped.
+        encode: `bool`, default True, whether to encode categorical values into integers.
+            If you've already encoded the categorical values to integers,
+            using Label/Ordinal encoding, you should set this to False,
+            otherwise leave it as True.
+            In the case of True, categorical values will be mapped to integers using
+            keras's string lookup layer.
     """
     def __init__(self,
                  categorical_features_vocabulary: dict,
                  embedding_dim: int = 32,
+                 encode: bool = False,
                  **kwargs):
         super().__init__(**kwargs)
         self.categorical_features_vocabulary = categorical_features_vocabulary
-        self.categorical_features_idx = list(self.categorical_features_vocabulary.keys())
         self.embedding_dim = embedding_dim
-        # self.lookup_tables = None
-        # self.embedding_layers = None
-        # self._prepped = False # Flag to indicate whether lookup tables and embeddings layers have been prepared
+        self.encode = encode
+
+        self.categorical_features = list(self.categorical_features_vocabulary.keys())
+
         self.lookup_tables, self.embedding_layers = self._get_lookup_tables_and_embedding_layers()
         self.concat = layers.Concatenate(axis=1)
         self.categorical_feature_indicator = None
+        self._is_data_in_dict_format = False
+        self._is_first_batch = True
 
     def _get_lookup_tables_and_embedding_layers(self):
         """Lookup tables and embedding layers for each categorical feature"""
-        # lookup_tables = {}
-        # embedding_layers = {}
         lookup_tables = []
         embedding_layers = []
-        for idx, (feature_name, unique_values) in self.categorical_features_vocabulary.items():
-        # for feature, vocabulary in self.categorical_features_vocabulary.items():
-        #     # Lookup Table to convert string values to integer indices
-        #     lookup = layers.StringLookup(vocabulary=unique_values,
-        #                                  mask_token=None,
-        #                                  num_oov_indices=0,
-        #                                  output_mode="int"
-        #                                  )
-        #     # lookup_tables[feature_name] = lookup
-        #     lookup_tables.append(lookup)
+        for feature_name, (feature_idx, unique_values) in self.categorical_features_vocabulary.items():
+            # If the categorical values in the dataset haven't been encoded in preprocessing step
+            # then encode them first. The `encode` parameter lets user specify if we need to encode
+            # the categorical values or not.
+            if self.encode:
+                # Lookup Table to convert string values to integer indices
+                lookup = layers.StringLookup(vocabulary=unique_values,
+                                             mask_token=None,
+                                             num_oov_indices=0,
+                                             output_mode="int"
+                                             )
+                lookup_tables.append(lookup)
 
             # Embedding layers map the integer representations of categorical values
             # to dense vectors of `embedding_dim` dimensionality,
-            # which in fancier lingo are called `embedddings`.
+            # which in fancier lingo are called `embeddings`.
             embedding = layers.Embedding(input_dim=len(unique_values),
                                          output_dim=self.embedding_dim)
-            # embedding_layers[feature_name] = embedding
             embedding_layers.append(embedding)
 
         return lookup_tables, embedding_layers
 
     def call(self, inputs):
+        # Find the dataset's format - is it either in dictionary format.
+        # If it's element_spec is an instance of dict, it's in dictionary format
+        # If it's element_spec is an instance of tuple, it's in array format
+        if self._is_first_batch:
+            if isinstance(inputs.element_spec, dict):
+                self._is_data_in_dict_format = True
+            self._is_first_batch = False
+
         # Encode and embedd categorical features
         dim = tf.shape(inputs)[1]
-        if self.categorical_feature_indicator is None:
-            self.categorical_feature_indicator = tf.zeros(shape=(dim,))
-            indices = tf.expand_dims(tf.constant(self.categorical_features_idx), axis=1)
-            self.categorical_feature_indicator = tf.tensor_scatter_nd_update(self.categorical_feature_indicator, indices, tf.ones(shape=(len(indices),)))
-
-        # feature_embeddings = []
-        feature_embeddings = tf.TensorArray(size=dim, dtype=tf.float32)
-        cat_index = 0
-        for idx in range(dim):
-            feature = tf.expand_dims(inputs[:, idx], 1)
-            # We only embedd the feature if it's categorical
-            # otherwise it is passed as is.
-            if self.categorical_feature_indicator[idx] == 1:
-                # Congrats, it's a boy... i mean categorical.
-                # feature_name = self.categorical_features_vocabulary[idx][0]
-                # lookup = self.lookup_tables[feature_name]
-                # lookup = self.lookup_tables[cat_index]
-                # embedding = self.embedding_layers[feature_name]
-                embedding = self.embedding_layers[cat_index]
+        categorical_feature_embeddings = tf.TensorArray(size=dim, dtype=tf.float32)
+        # feature_idx is the overall index of feature in the dataset
+        # so it can't be used to retrieve lookup table and embedding layer from list
+        # which are both of length equal to number of categorical features - not input dimensions
+        # hence the need for current_idx
+        current_idx = 0
+        # We only embedd the categorical features
+        for feature_name, (feature_idx, _) in self.categorical_features_vocabulary.items():
+            if self._is_data_in_dict_format:
+                feature = tf.expand_dims(inputs[feature_name], 1)
+            else:
+                feature = tf.expand_dims(inputs[:, feature_idx], 1)
+            if self.encode:
                 # Convert string input values to integer indices
-                # feature = lookup(feature)
-                # Convert index values to embedding representations
-                feature = embedding(feature)
-                feature = tf.squeeze(feature, axis=-1)
-                cat_index += 1
+                lookup = self.lookup_tables[current_idx]
+                feature = lookup(feature)
+            # Convert index values to embedding representations
+            embedding = self.embedding_layers[current_idx]
+            feature = embedding(feature)
+            feature = tf.squeeze(feature, axis=-1)
+            categorical_feature_embeddings = categorical_feature_embeddings.write(current_idx, feature)
+            current_idx += 1
 
-            # feature_embeddings.append(feature)
-            feature_embeddings = feature_embeddings.write(idx, feature)
-
-        # feature_embeddings = self.concat(feature_embeddings)
-        feature_embeddings = tf.transpose(tf.squeeze(feature_embeddings.stack()))
-        return feature_embeddings
+        categorical_feature_embeddings = tf.transpose(tf.squeeze(categorical_feature_embeddings.stack()))
+        return categorical_feature_embeddings
