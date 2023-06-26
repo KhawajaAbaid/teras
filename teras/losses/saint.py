2e6268470a57a8646c008a76214ec75e4f86edb1
abaid <khawaja.abaid@gmail.com>
2023-06-26 16:30:12 +0500

    Implement denoising loss for SAINT pretrainer

--- teras/losses/saint.py
+++ teras/losses/saint.py
@@ -33,3 +33,46 @@ def info_nce_loss(projection_outputs_original=None,
     return loss
 
 
+def denoising_loss(real_samples=None,
+                   reconstructed_samples=None,
+                   num_categorical_features: int = None):
+    """
+    Since we apply categorical and numerical embedding layers
+    separately and then combine them into a new features matrix
+    this effectively makes the first k features in the outputs
+    categorical (since categorical embeddings are applied first)
+    and all other features numerical.
+    Here, k = num_categorical_features
+
+    Args:
+        real_samples: `tf.Tensor`,
+            Samples drawn from the original dataset.
+        reconstructed_samples: `tf.Tensor`,
+            Samples reconstructed by the reconstruction head.
+        num_categorical_features: `int`,
+            Number of categorical features in the dataset.
+            If there are no categorical features, specify 0.
+    """
+    if real_samples is None:
+        raise ValueError("`real_samples` cannot be None. "
+                         "You must pass the samples drawn from the original dataset. ")
+
+    if reconstructed_samples is None:
+        raise ValueError("`reconstructed_samples` cannot be None. "
+                         "You must pass the samples reconstructed by the ReconstructionHead.")
+
+    if num_categorical_features is None:
+        raise ValueError("`num_categorical_features` cannot be None. "
+                         "If there are no categorical features in the dataset, pass 0.")
+
+    num_features = tf.shape(real_samples)[1]
+    loss = 0.
+    if num_categorical_features > 0:
+        loss += tf.reduce_sum(tf.losses.categorical_crossentropy(real_samples,
+                                                                 reconstructed_samples,
+                                                                 from_logits=True))
+    if num_categorical_features < num_features:
+        # there are numerical features
+        loss += tf.reduce_sum(tf.losses.mse(real_samples, reconstructed_samples))
+
+    return loss
