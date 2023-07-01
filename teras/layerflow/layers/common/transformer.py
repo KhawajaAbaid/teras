113bd5ae4bc5dc97a54fb7a1292111e40d5d687f
abaid <khawaja.abaid@gmail.com>
2023-07-01 12:54:22 +0500

    LayerFlow API: layers/common/transformer: Implement common `Transformer` and `Encoder` layers

---
+++ teras/layerflow/layers/common/transformer.py
@@ -0,0 +1,57 @@
+from tensorflow import keras
+from tensorflow.keras import layers, models
+from teras.layers.common.transformer import (Transformer as BaseTransformer,
+                                             Encoder as BaseEncoder)
+from typing import List
+
+LIST_OF_LAYERS = List[keras.layers.Layer]
+
+
+class Transformer(BaseTransformer):
+    """
+    Transformer layer with LayerFlow design.
+    It supposed to be used when user wants additional flexibility
+    to customize the MultiHeadAttention and FeedForward layers,
+    that is not offered by the default Teras API.
+
+    Args:
+        multi_head_attention: `layers.Layer`,
+            An instance of MultiHeadAttention layer or any layer
+            that can be sued in place of MultiHeadAttention layer.
+        feed_forward: `layers.Layer`,
+            An instance of FeedForward layer or any layer that
+            can be used in place of FeedForward layer.
+    """
+    def __init__(self,
+                 multi_head_attention: layers.Layer = None,
+                 feed_forward: layers.Layer = None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        if multi_head_attention is not None:
+            self.multi_head_attention = multi_head_attention
+
+        if feed_forward is not None:
+            self.feed_forward = feed_forward
+
+
+class Encoder(BaseEncoder):
+    """
+    Encoder layer with LayerFlow design.
+    It supposed to be used when user wants additional flexibility
+    to customize the Transformer layers,
+    that is not offered by the default Teras API.
+
+    Args:
+        transformer_layers: `List[layers.Layer]`,
+            A list of Transformer layers that make up the encoder
+            layer.
+    """
+    def __init__(self,
+                 transformer_layers: LIST_OF_LAYERS = None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        if transformer_layers is not None:
+            self.transformer_layers = models.Sequential(
+                transformer_layers,
+                name="transformer_layers"
+            )
