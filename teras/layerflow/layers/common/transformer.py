5819a7303f9143399fffc7949f8e3c1a50ac012a
abaid <khawaja.abaid@gmail.com>
2023-07-01 17:23:01 +0500

    LayerFlow API: Implement Classification and Regression heads based on LayerFlow design

--- teras/layerflow/layers/common/transformer.py
+++ teras/layerflow/layers/common/transformer.py
@@ -1,7 +1,9 @@
 from tensorflow import keras
 from tensorflow.keras import layers, models
 from teras.layers.common.transformer import (Transformer as BaseTransformer,
-                                             Encoder as BaseEncoder)
+                                             Encoder as BaseEncoder,
+                                             RegressionHead as BaseRegressionHead,
+                                             ClassificationHead as BaseClassificationHead)
 from typing import List
 
 LIST_OF_LAYERS = List[keras.layers.Layer]
@@ -55,3 +57,66 @@ class Encoder(BaseEncoder):
                 transformer_layers,
                 name="transformer_layers"
             )
+
+
+# TODO: Append these Base.* layer names with an underscore,
+#       so it becomes cleaner when the user tries to import layers
+
+class ClassificationHead(BaseClassificationHead):
+    """
+    ClassificationHead with LayerFlow design.
+    It's purposed to be used on top of the transformer based
+    architectures for classification.
+
+    Args:
+        hidden_block: `layers.Layer | models.Model`,
+            An instance of anything that can serve as the hidden block in the
+            classification head.
+            It can be as simple as a single dense layer, or a custom layer that
+            uses a bunch of other dense and other fancy layers,
+            or may as well be a keras model -- as long as it satisfies the input
+            output constraints. (All thanks to Keras and Francois Chollet :D)
+        output_layer: `layers.Layer`,
+            An instance of keras layer (Dense or a custom layer), with relevant
+            activation function for classification relevant to the task at hand.
+    """
+    def __init__(self,
+                 hidden_block: layers.Layer = None,
+                 output_layer: layers.Layer = None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        if hidden_block is not None:
+            self.hidden_block = hidden_block
+
+        if output_layer is not None:
+            self.output_layer = output_layer
+
+
+class RegressionHead(BaseRegressionHead):
+    """
+    RegressionHead with LayerFlow design.
+    It's purposed to be used on top of the transformer based
+    architectures for regression.
+
+    Args:
+        hidden_block: `layers.Layer | models.Model`,
+            An instance of anything that can serve as the hidden block in the
+            regression head.
+            It can be as simple as a single dense layer, or a custom layer that
+            uses a bunch of other dense and other fancy layers,
+            or may as well be a keras model -- as long as it satisfies the input
+            output constraints. (All thanks to Keras and Francois Chollet :D)
+        output_layer: `layers.Layer`,
+            An instance of keras layer (Dense or a custom layer),
+            for regression outputs relevant to the task at hand.
+    """
+    def __init__(self,
+                 hidden_block: layers.Layer = None,
+                 output_layer: layers.Layer = None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        if hidden_block is not None:
+            self.hidden_block = hidden_block
+
+        if output_layer is not None:
+            self.output_layer = output_layer
