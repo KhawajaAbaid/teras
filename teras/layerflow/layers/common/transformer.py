850e25358f9a208ea6b52b348ac46924bd5f00ca
abaid <khawaja.abaid@gmail.com>
2023-07-03 18:16:09 +0500

    LayerFlow: Implement FeedForward layer

--- teras/layerflow/layers/common/transformer.py
+++ teras/layerflow/layers/common/transformer.py
@@ -1,10 +1,42 @@
 from tensorflow import keras
 from tensorflow.keras import layers, models
 from teras.layers.common.transformer import (Transformer as _BaseTransformer,
-                                             Encoder as _BaseEncoder)
+                                             Encoder as _BaseEncoder,
+                                             FeedForward as _BaseFeedForward)
 from typing import List
 
 LIST_OF_LAYERS = List[keras.layers.Layer]
+
+
+class FeedForward(_BaseFeedForward):
+    """
+    FeedForward layer with LayerFlow design.
+    It is used in the Transformer layer of the Encoder block
+    of Transformer architecture.
+    It is shared by all transformer based architectures for tabular data.
+
+    Args:
+        hidden_block: `layers.Layer`,
+            A dense layer or any custom layer that can serve as the hidden
+            block for the FeedForward layer.
+            If None, a hidden block with default values is used.
+        output_layer: `layers.Layer`,
+            A dense layer or any custom layer that projects data back to
+            the input/embedding dimensions. Since the FeedForward recieves
+            feature embeddings as inputs, it after apply hidden block,
+            projects them back to the embedding dimensions.
+            If None, an output layer with default value is used.
+    """
+    def __init__(self,
+                 hidden_block: layers.Layer = None,
+                 output_layer: layers.Layer = None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        if hidden_block is not None:
+            self.hidden_block = hidden_block
+
+        if output_layer is not None:
+            self.output_layer = output_layer
 
 
 class Transformer(_BaseTransformer):
