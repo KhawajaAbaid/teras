886fd440d554ab5790f77d749fddbee47d316cf1
abaid <khawaja.abaid@gmail.com>
2023-07-05 22:39:43 +0500

    LayerFlow: Implement RegessionHead and ClassificationHead layers for DNFNet

--- teras/layerflow/layers/dnfnet.py
+++ teras/layerflow/layers/dnfnet.py
@@ -1,10 +1,13 @@
-from tensorflow.keras import layers
+from tensorflow.keras import layers, models
 from teras.layers.dnfnet import (FeatureSelection,
                                  Localization,
-                                 DNNF as _BaseDNNF)
-from typing import List
+                                 DNNF as _BaseDNNF,
+                                 ClassificationHead as _BaseClassificationHead,
+                                 RegressionHead as _BaseRegressionHead)
+from typing import List, Union
 
 LIST_OF_INT = List[int]
+LAYER_OR_MODEL = Union[layers.Layer, models.Model]
 
 
 class DNNF(_BaseDNNF):
@@ -55,3 +58,65 @@ class DNNF(_BaseDNNF):
 
         if localization is not None:
             self.localization = localization
+
+
+class ClassificationHead(_BaseClassificationHead):
+    """
+    ClassificationHead with LayerFlow design for DNFNet.
+
+    Args:
+        hidden_block: `layers.Layer | models.Model`,
+            An instance of anything that can serve as the hidden block in the
+            classification head.
+            It can be as simple as a single dense layer, or a custom layer that
+            uses a bunch of other dense and other fancy layers,
+            or may as well be a keras model -- as long as it satisfies the input
+            output constraints.
+            If None, a default hidden block specific to the current architecture
+            will be used.
+        output_layer: `layers.Layer`,
+            An instance of keras layer (Dense or a custom layer), with relevant
+            activation function for classification relevant to the task at hand.
+            If None, a default relevant output layer will be used.
+    """
+    def __init__(self,
+                 hidden_block: LAYER_OR_MODEL = None,
+                 output_layer: layers.Layer = None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        if hidden_block is not None:
+            self.hidden_block = hidden_block
+
+        if output_layer is not None:
+            self.output_layer = output_layer
+
+
+class RegressionHead(_BaseRegressionHead):
+    """
+    RegressionHead with LayerFlow design for DNFNet.
+
+    Args:
+        hidden_block: `layers.Layer | models.Model`,
+            An instance of anything that can serve as the hidden block in the
+            regression head.
+            It can be as simple as a single dense layer, or a custom layer that
+            uses a bunch of other dense and other fancy layers,
+            or may as well be a keras model -- as long as it satisfies the input
+            output constraints.
+            If None, a default hidden block specific to the current architecture
+            will be used.
+        output_layer: `layers.Layer`,
+            An instance of keras layer (Dense or a custom layer),
+            for regression outputs relevant to the task at hand.
+            If None, a default relevant output layer will be used.
+    """
+    def __init__(self,
+                 hidden_block: LAYER_OR_MODEL = None,
+                 output_layer: layers.Layer = None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        if hidden_block is not None:
+            self.hidden_block = hidden_block
+
+        if output_layer is not None:
+            self.output_layer = output_layer
