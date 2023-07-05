601b07c532f9045e8f8e8735cfa73a0e871fd215
abaid <khawaja.abaid@gmail.com>
2023-07-05 11:25:54 +0500

    Update RTDL ResNet models: Create a base ResNet model and make classifier and regressor inherit from it. Update docstrings, params

--- teras/models/rtdl_resnet.py
+++ teras/models/rtdl_resnet.py
@@ -8,185 +8,193 @@ from teras.layers import (RTDLResNetBlock,
 LayerType = Union[str, keras.layers.Layer]
 
 
-class ResNetClassifier(keras.Model):
+class RTDLResNet(keras.Model):
     """
-    The ResNet Classifier model based on the architecture proposed by Yury Gorishniy et al.
+    The ResNet model based on the architecture proposed by Yury Gorishniy et al.
     in the paper Revisiting Deep Learning Models for Tabular Data.
 
     Reference(s):
         https://arxiv.org/abs/2106.11959
 
     Args:
-        num_classes: Number of classes to predict
-        num_blocks: Number of ResNet blocks to use.
-        main_dim: Main dimensionality. This is the dimensionality of the inputs
-            expected by the ResNet block and the output of this block will be of this dimensionality.
-        hidden_dim: Dimensionality of the hidden layer in the ResNet block.
-        dropout_main: Dropout rate to use for the dropout layer that is applied
+        num_blocks: `int`, default 4,
+            Number of ResNet blocks to use.
+        block_units: `int`, default 64,
+            Dimensionality of the hidden layer in the ResNet block.
+        block_dropout_hidden: `float`, default 0.,
+            Dropout rate to use for the dropout layer that is applied
             after the hidden dense layer.
-        dropout_main: Dropout rate to use for the dropout layer that is applied
-            after the second and last 'main' dense layer.
-        bias_hidden: Whether to use bias in the hidden dense layer
-        bias_main: Whether to use bias in the output or main dense layer.
-        activation_hidden: Activation to use bias in the hidden dense layer
-        activation_main: Activation to use bias in the main dense layer
-        normalization_block: Normalization layer to normalize
-            the inputs to the RestNet block. Defaults to BatchNormalization.
-        use_skip_connection: Whether to use the skip connection in the ResNet block
-        activation_out: Output activation, by default, sigmoid is used for binary
-            while softmax is used for multiclass classification
-        normalization_head: Normalization layer to use in the classification head
+        block_dropout_out: `float`, default 0.,
+            Dropout rate to use for the dropout layer that is applied
+            after the output dense layer.
+        block_activation_hidden: default "relu",
+            Activation function to use in the hidden layer.
+        block_activation_out: default "relu",
+            Activation function to use in the output layer.
+        normalization: default "BatchNormalization",
+            Normalization layer to normalize the inputs to the RestNet block.
+        use_skip_connection: `bool`, default True,
+            Whether to use the skip connection in the ResNet block.
     """
     def __init__(self,
-                 num_classes: int = None,
-                 num_blocks: int = 1,
-                 main_dim: int = None,
-                 hidden_dim: int = None,
-                 dropout_main: float = 0.,
+                 num_blocks: int = 4,
+                 units: int = 64,
                  dropout_hidden: float = 0.,
-                 activation_main: LayerType = "relu",
+                 dropout_out: float = 0.,
                  activation_hidden: LayerType = "relu",
-                 normalization_block: LayerType = "BatchNormalization",
+                 activation_out: LayerType = "relu",
+                 normalization: LayerType = "BatchNormalization",
                  use_skip_connection: bool = True,
-                 activation_out: LayerType = None,
-                 normalization_head: LayerType = "BatchNormalization",
                  **kwargs):
         super().__init__(**kwargs)
-        self.num_classes = 1 if num_classes <= 2 else num_classes
         self.num_blocks = num_blocks
-        self.main_dim = main_dim
-        self.hidden_dim = hidden_dim
-        self.dropout_main = dropout_main
+        self.units = units
         self.dropout_hidden = dropout_hidden
-        self.normalization_block = normalization_block
-        self.activation_main = activation_main
+        self.dropout_out = dropout_out
         self.activation_hidden = activation_hidden
-        self.use_skip_connection = use_skip_connection
         self.activation_out = activation_out
-        self.normalization_head = normalization_head
+        self.normalization = normalization
+        self.use_skip_connection = use_skip_connection
 
-        self.head = RTDLResNetClassificationHead(num_classes=self.num_classes,
-                                                 activation_out=self.activation_out,
-                                                 normalization=self.normalization_head)
-
-    def build(self, input_shape):
-        # Keeping in accordance with the official implementation,
-        # when the user doesn't specify main_dim, it will be
-        # set equal to the input dimensionality
-        if self.main_dim is None:
-            self.main_dim = input_shape[1]
-
-        self.first_layer = keras.layers.Dense(self.main_dim)
-
-        self.blocks = keras.models.Sequential(
+        self.resnet_blocks = keras.models.Sequential(
             [
                 RTDLResNetBlock(
-                    hidden_dim=self.hidden_dim,
-                    main_dim=self.main_dim,
-                    dropout_hidden=self.dropout_hidden,
-                    dropout_main=self.dropout_main,
-                    activation_hidden=self.activation_hidden,
-                    activation_main=self.activation_main,
+                    units=self.units,
+                    dropout=self.dropout,
+                    activation=self.activation,
                     normalization=self.normalization_block,
                     use_skip_connection=self.use_skip_connection
 
                 )
                 for _ in range(self.num_blocks)
-            ], name="ResNetRTDL_Body"
+            ], name="resnet_blocks"
         )
 
     def call(self, inputs):
-        x = self.first_layer(inputs)
-        x = self.blocks(x)
-        x = self.head(x)
-        return x
+        outputs = self.resnet_blocks(inputs)
+        if self.head is not None:
+            outputs = self.head(outputs)
+        return outputs
+
+
+class RTDLResNetClassifier(RTDLResNet):
+    """
+    The ResNet Classifier model based on the ResNet architecture
+    proposed by Yury Gorishniy et al.
+    in the paper Revisiting Deep Learning Models for Tabular Data.
+
+    Reference(s):
+        https://arxiv.org/abs/2106.11959
+
+    Args:
+        num_classes: `int`, default 2,
+            Number of classes to predict
+        activation_out:
+            Activation function to use in the output layer.
+            By default, "sigmoid" is used for binary while "softmax" is used for
+            multiclass classification.
+        num_blocks: `int`, default 4,
+            Number of ResNet blocks to use.
+        units: `int`, default 64,
+            Dimensionality of the hidden layer in the ResNet block.
+        dropout_hidden: `float`, default 0.,
+            Dropout rate to use for the dropout layer that is applied
+            after the hidden dense layer of each ResNet block.
+        dropout_out: `float`, default 0.,
+            Dropout rate to use for the dropout layer that is applied
+            after the output dense layer of each ResNet block.
+        activation_hidden: default "relu",
+            Activation function to use in the hidden layer of each ResNet block.
+        activation_out_block: default "relu",
+            Activation function to use in the output layer of each ResNet block.
+        normalization: default "BatchNormalization",
+            Normalization layer to normalize the inputs to the RestNet block.
+        use_skip_connection: `bool`, default True,
+            Whether to use the skip connection in each ResNet block.
+    """
+    def __init__(self,
+                 num_classes: int = 2,
+                 activation_out=None,
+                 num_blocks: int = 4,
+                 units: int = 64,
+                 dropout_hidden: float = 0.,
+                 dropout_out: float = 0.,
+                 activation_hidden: LayerType = "relu",
+                 activation_out_block: LayerType = "relu",
+                 normalization: LayerType = "BatchNormalization",
+                 use_skip_connection: bool = True,
+                 **kwargs):
+        super().__init__(num_blocks=num_blocks,
+                         units=units,
+                         dropout_hidden=dropout_hidden,
+                         dropout_out=dropout_out,
+                         activation_hidden=activation_hidden,
+                         activation_out=activation_out_block,
+                         normalization=normalization,
+                         use_skip_connection=use_skip_connection,
+                         **kwargs)
+
+        self.num_classes = 1 if num_classes <= 2 else num_classes
+        self.activation_out = activation_out
+        if self.activation_out is None:
+            self.activation_out = "sigmoid" if self.num_classes == 1 else "softmax"
+
+        self.head = RTDLResNetClassificationHead(num_classes=self.num_classes,
+                                                 activation_out=self.activation_out,
+                                                 name="rtdl_resnet_classification_head")
 
 
 class ResNetRegressor(keras.Model):
     """
-    The ResNet Classifier model based on the architecture proposed by Yury Gorishniy et al.
+    The ResNet Regressor model based on the ResNet architecture
+    proposed by Yury Gorishniy et al.
     in the paper Revisiting Deep Learning Models for Tabular Data.
 
     Reference(s):
         https://arxiv.org/abs/2106.11959
 
     Args:
-        units_out: Number of regression outputs
-        num_blocks: Number of ResNet blocks to use.
-        main_dim: Main dimensionality. This is the dimensionality of the inputs
-            expected by the ResNet block and the output of this block will be of this dimensionality.
-        hidden_dim: Dimensionality of the hidden layer in the ResNet block.
-        dropout_main: Dropout rate to use for the dropout layer that is applied
-            after the hidden dense layer.
-        dropout_main: Dropout rate to use for the dropout layer that is applied
-            after the second and last 'main' dense layer.
-        bias_hidden: Whether to use bias in the hidden dense layer
-        bias_main: Whether to use bias in the output or main dense layer.
-        activation_hidden: Activation to use bias in the hidden dense layer
-        activation_main: Activation to use bias in the main dense layer
-        normalization_block: Normalization layer to normalize
-            the inputs to the RestNet block. Defaults to BatchNormalization.
-        use_skip_connection: Whether to use the skip connection in the ResNet block
-        normalization_head: Normalization layer to use in the classification head
+        num_outputs: `int`, default 1,
+            Number of regression outputs
+        num_blocks: `int`, default 4,
+            Number of ResNet blocks to use.
+        units: `int`, default 64,
+            Dimensionality of the hidden layer in the ResNet block.
+        dropout_hidden: `float`, default 0.,
+            Dropout rate to use for the dropout layer that is applied
+            after the hidden dense layer of each ResNet block.
+        dropout_out: `float`, default 0.,
+            Dropout rate to use for the dropout layer that is applied
+            after the output dense layer of each ResNet block.
+        activation_hidden: default "relu",
+            Activation function to use in the hidden layer of each ResNet block.
+        activation_out_block: default "relu",
+            Activation function to use in the output layer of each ResNet block.
+        normalization: default "BatchNormalization",
+            Normalization layer to normalize the inputs to the RestNet block.
+        use_skip_connection: `bool`, default True,
+            Whether to use the skip connection in each ResNet block.
     """
-
     def __init__(self,
-                 units_out: int = 1,
-                 num_blocks: int = 1,
-                 main_dim: int = None,
-                 hidden_dim: int = None,
-                 dropout_main: float = 0.,
+                 num_outputs: int = 1,
+                 num_blocks: int = 4,
+                 units: int = 64,
                  dropout_hidden: float = 0.,
-                 activation_main: LayerType = "relu",
+                 dropout_out: float = 0.,
                  activation_hidden: LayerType = "relu",
-                 normalization_block: LayerType = "BatchNormalization",
+                 activation_out_block: LayerType = "relu",
+                 normalization: LayerType = "BatchNormalization",
                  use_skip_connection: bool = True,
-                 normalization_head: LayerType = "BatchNormalization",
                  **kwargs):
-        super().__init__(**kwargs)
-        self.units_out = units_out
-        self.num_blocks = num_blocks
-        self.main_dim = main_dim
-        self.hidden_dim = hidden_dim
-        self.dropout_main = dropout_main
-        self.dropout_hidden = dropout_hidden
-        self.normalization_block = normalization_block
-        self.activation_main = activation_main
-        self.activation_hidden = activation_hidden
-        self.use_skip_connection = use_skip_connection
-        self.normalization_head = normalization_head
-
-        self.head = RTDLResNetRegressionHead(units_out=self.units_out,
+        super().__init__(num_blocks=num_blocks,
+                         units=units,
+                         dropout_hidden=dropout_hidden,
+                         dropout_out=dropout_out,
+                         activation_hidden=activation_hidden,
+                         activation_out=activation_out_block,
+                         normalization=normalization,
+                         use_skip_connection=use_skip_connection,
+                         **kwargs)
+        self.num_outputs = num_outputs
+        self.head = RTDLResNetRegressionHead(num_outputs=self.num,
                                              normalization=self.normalization_head)
-
-    def build(self, input_shape):
-        # Keeping in accordance with the official implementation,
-        # when the user doesn't specify main_dim, it will be
-        # set equal to the input dimensionality
-        if self.main_dim is None:
-            self.main_dim = input_shape[1]
-
-        self.first_layer = keras.layers.Dense(self.main_dim)
-
-        self.blocks = keras.models.Sequential(
-            [
-                RTDLResNetBlock(
-                    hidden_dim=self.hidden_dim,
-                    main_dim=self.main_dim,
-                    dropout_hidden=self.dropout_hidden,
-                    dropout_main=self.dropout_main,
-                    activation_hidden=self.activation_hidden,
-                    activation_main=self.activation_main,
-                    normalization=self.normalization_block,
-                    use_skip_connection=self.use_skip_connection
-
-                )
-                for _ in range(self.num_blocks)
-            ], name="ResNetRTDL_Body"
-        )
-
-    def call(self, inputs):
-        x = self.first_layer(inputs)
-        x = self.blocks(x)
-        x = self.head(x)
-        return x
