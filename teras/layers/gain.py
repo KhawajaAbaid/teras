f29f0dc296d9f90f198502be2ff53ff1a46e0f75
abaid <khawaja.abaid@gmail.com>
2023-07-06 13:00:02 +0500

    Update DiscriminatorBlock and GeneratorBlock layers -- add docstrings and update params

--- teras/layers/gain.py
+++ teras/layers/gain.py
@@ -1,30 +1,61 @@
-from tensorflow import keras
 from tensorflow.keras import layers
 
 
 class GeneratorBlock(layers.Layer):
+    """
+    GeneratorBlock that is used in construction of hidden block
+    inside the Generator model of GAIN architecture.
+
+    Args:
+        units: `int`,
+            Hidden dimensionality of the generator block
+        activation: default "relu",
+            Activation function to use in the generator block
+        kernel_initializer: default "glorot_normal",
+            Kernel initializer for the generator block
+    """
     def __init__(self,
-                 units,
+                 units: int,
+                 activation="relu",
+                 kernel_initializer="glorot_normal",
                  **kwargs):
         super().__init__(**kwargs)
         self.units = units
+        self.activation = activation
+        self.kernel_initializer = kernel_initializer
         self.dense = layers.Dense(units,
-                                  activation="relu",
-                                  kernel_initializer="glorot_normal")
+                                  activation=self.activation,
+                                  kernel_initializer=self.kernel_initializer)
 
     def call(self, inputs):
         return self.dense(inputs)
 
 
 class DiscriminatorBlock(layers.Layer):
+    """
+    DiscriminatorBlock that is used in construction of hidden block
+    inside the Discriminator model of GAIN architecture.
+
+    Args:
+        units: `int`,
+            Hidden dimensionality of the discriminator block
+        activation: default "relu",
+            Activation function to use in the discriminator block
+        kernel_initializer: default "glorot_normal",
+            Kernel initializer for the discriminator block
+    """
     def __init__(self,
-                 units,
+                 units: int,
+                 activation="relu",
+                 kernel_initializer="glorot_normal",
                  **kwargs):
         super().__init__(**kwargs)
         self.units = units
+        self.activation = activation
+        self.kernel_initializer = kernel_initializer
         self.dense = layers.Dense(units,
-                                  activation="relu",
-                                  kernel_initializer="glorot_normal")
+                                  activation=self.activation,
+                                  kernel_initializer=self.kernel_initializer)
 
     def call(self, inputs):
         return self.dense(inputs)
