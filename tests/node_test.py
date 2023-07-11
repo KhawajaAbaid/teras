c8cae2363cb52ef016a2f8874068bb342f050b01
abaid <khawaja.abaid@gmail.com>
2023-07-11 12:36:26 +0500

    Update node test

--- tests/node_test.py
+++ tests/node_test.py
@@ -8,8 +8,8 @@ from sklearn import datasets as sklearn_datasets
 # <<<<<<<<<<<<<<<<<<< REGRESSION Test >>>>>>>>>>>>>>>>>>>>>>>>>>
 (train_data, train_targets), (test_data, test_target) = (boston_housing.load_data())
 print(f"{'-'*15} REGRESSION TEST {'-'*15}")
-node_regressor = NODERegressor(n_trees=16,
-                                n_layers=8)
+node_regressor = NODERegressor(num_trees=16,
+                                num_layers=8)
 
 node_regressor.compile(loss="MSE", optimizer=keras.optimizers.Adam(learning_rate=0.05), metrics=["MAE"])
 node_regressor.fit(train_data, train_targets, batch_size=32, epochs=10)
@@ -23,8 +23,8 @@ iris_dataset = sklearn_datasets.load_iris()
 X = iris_dataset.data
 y = iris_dataset.target
 
-node_classifier = NODEClassifier(n_trees=16,
-                                 n_classes=3,
-                                 n_layers=8)
+node_classifier = NODEClassifier(num_trees=16,
+                                 num_classes=3,
+                                 num_layers=8)
 node_classifier.compile(loss=keras.losses.SparseCategoricalCrossentropy(), optimizer=keras.optimizers.Adam(learning_rate=0.01), metrics=["accuracy"])
 node_classifier.fit(X, y, batch_size=32, epochs=10)
