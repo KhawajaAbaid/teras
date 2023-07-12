57dd8c688758655201f2c88f61785114bc33de60
abaid <khawaja.abaid@gmail.com>
2023-07-12 19:53:57 +0500

    Implement unit test for Generator and Discriminator layers

---
+++ teras/layers/ctgan_test.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from teras.layers.ctgan import GeneratorBlock, DiscriminatorBlock
+
+
+def test_generator_block_output_shape():
+    gen_block = GeneratorBlock(units=32)
+    inputs = tf.ones((128, 16), dtype=tf.float32)
+    outputs = gen_block(inputs)
+    assert len(tf.shape(outputs)) == 2
+    assert tf.shape(outputs)[0] == 128
+    assert tf.shape(outputs)[1] == 32 + 16
+
+
+def test_discriminator_block_output_shape():
+    gen_block = DiscriminatorBlock(units=32)
+    inputs = tf.ones((128, 16), dtype=tf.float32)
+    outputs = gen_block(inputs)
+    assert len(tf.shape(outputs)) == 2
+    assert tf.shape(outputs)[0] == 128
+    assert tf.shape(outputs)[1] == 32
