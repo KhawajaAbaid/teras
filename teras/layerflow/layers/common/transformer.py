68d45300d1bb82d2e5e7d40bf133b2e22572f9ac
abaid <khawaja.abaid@gmail.com>
2023-07-21 23:50:18 +0500

    Redesign common transformer layers' LayerFlow versions

--- teras/layerflow/layers/common/transformer.py
+++ teras/layerflow/layers/common/transformer.py
@@ -1,111 +1,113 @@
 from tensorflow import keras
-from tensorflow.keras import layers, models
-from teras.layers.common.transformer import (Transformer as _BaseTransformer,
-                                             Encoder as _BaseEncoder,
-                                             FeedForward as _BaseFeedForward)
+from teras.layerflow.layers.common.common import HiLOL
+from teras.utils import (serialize_layers_collection,
+                         deserialize_layers_collection)
 from typing import List
 
 LIST_OF_LAYERS = List[keras.layers.Layer]
 
 
-class FeedForward(_BaseFeedForward):
+class FeedForward(HiLOL):
     """
     FeedForward layer with LayerFlow design.
-    It is used in the Transformer layer of the Encoder block
+    It is used in the Transformer layer of the ``Encoder`` layer
     of Transformer architecture.
     It is shared by all transformer based architectures for tabular data.
 
     Args:
-        hidden_block: `layers.Layer`,
+        hidden_block: ``keras.layers.Layer``,
             A dense layer or any custom layer that can serve as the hidden
-            block for the FeedForward layer.
-            If None, a hidden block with default values is used.
-        output_layer: `layers.Layer`,
+            block for the ``FeedForward`` layer.
+
+        output_layer: ``keras.layers.Layer``,
             A dense layer or any custom layer that projects data back to
-            the input/embedding dimensions. Since the FeedForward recieves
+            the input/embedding dimensions. Since the ``FeedForward`` receives
             feature embeddings as inputs, it after apply hidden block,
             projects them back to the embedding dimensions.
-            If None, an output layer with default value is used.
     """
     def __init__(self,
-                 hidden_block: layers.Layer = None,
-                 output_layer: layers.Layer = None,
+                 hidden_block: keras.layers.Layer,
+                 output_layer: keras.layers.Layer,
                  **kwargs):
-        super().__init__(**kwargs)
-        if hidden_block is not None:
-            self.hidden_block = hidden_block
-
-        if output_layer is not None:
-            self.output_layer = output_layer
+        super().__init__(hidden_block=hidden_block,
+                         output_layer=output_layer,
+                         **kwargs)
 
 
-class Transformer(_BaseTransformer):
+class Transformer(keras.layers.Layer):
     """
     Transformer layer with LayerFlow design.
-    It supposed to be used when user wants additional flexibility
-    to customize the MultiHeadAttention and FeedForward layers,
-    that is not offered by the default Teras API.
+    It is used to made up the ``Encoder`` layer of the given
+    Transformer-based architecture.
 
     Args:
-        multi_head_attention: `layers.Layer`,
-            An instance of MultiHeadAttention layer or any layer
-            that can be sued in place of MultiHeadAttention layer.
-        feed_forward: `layers.Layer`,
-            An instance of FeedForward layer or any layer that
-            can be used in place of FeedForward layer.
-            You can import the `FeedForward` layer as follows,
+        multi_head_attention: ``keras.layers.Layer``,
+            An instance of ``MultiHeadAttention`` layer or any layer
+            that can be sued in place of ``MultiHeadAttention`` layer.
+
+        feed_forward: ``keras.layers.Layer``,
+            An instance of ``FeedForward`` layer or any layer that
+            can be used in its place.
+            You can import the ``FeedForward`` layer as follows,
                 >>> from teras.layerflow.layers import FeedForward
-                or
-                >>> from teras.layers import FeedForward
     """
     def __init__(self,
-                 multi_head_attention: layers.Layer = None,
-                 feed_forward: layers.Layer = None,
+                 multi_head_attention: keras.layers.Layer,
+                 feed_forward: keras.layers.Layer,
                  **kwargs):
         super().__init__(**kwargs)
-        if multi_head_attention is not None:
-            self.multi_head_attention = multi_head_attention
+        self.multi_head_attention = multi_head_attention
+        self.feed_forward = feed_forward
 
-        if feed_forward is not None:
-            self.feed_forward = feed_forward
+    def get_config(self):
+        config = super().get_config()
+        new_config = {'multi_head_attention': keras.layers.serialize(self.multi_head_attention),
+                      'feed_forward': keras.layers.serialize(self.feed_forward),
+                      }
+        config.update(new_config)
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        multi_head_attention = keras.layers.deserialize(config.pop("multi_head_attention"))
+        feed_forward = keras.layers.deserialize(config.pop("feed_forward"))
+        transformer = keras.layers.deserialize(config.pop("transformer"))
+        return cls(multi_head_attention=multi_head_attention,
+                   feed_forward=feed_forward,
+                   transformer=transformer,
+                   **config)
 
 
-class Encoder(_BaseEncoder):
+class Encoder(keras.layers.Layer):
     """
     Encoder layer with LayerFlow design.
-    It supposed to be used when user wants additional flexibility
-    to customize the Transformer layers,
-    that is not offered by the default Teras API.
+    It is made up of several ``Transformer`` layers and encodes the
+    input embeddings to useful representations that can then be used
+    for the task at hand, such as classificaiton or regression.
 
     Args:
-        transformer_layers: `List[layers.Layer]`,
-            A list of `Transformer` layers that make up the encoder
+        transformer_layers: ``List[keras.layers.Layer]``,
+            A list of ``Transformer`` layers that make up the encoder
             layer.
-            You can import the `Transformer` layer as follows,
+            You can import the ``Transformer`` layer as follows,
                 >>> from teras.layerflow.layers import Transformer
-                or
-                >>> from teras.layers import Transformer
     """
     def __init__(self,
-                 transformer_layers: LIST_OF_LAYERS = None,
+                 transformer_layers: LIST_OF_LAYERS,
                  **kwargs):
         super().__init__(**kwargs)
         if transformer_layers is not None:
-            self.transformer_layers = models.Sequential(
+            self.transformer_layers = keras.models.Sequential(
                 transformer_layers,
                 name="transformer_layers"
             )
 
+    def get_config(self):
+        config = super().get_config()
+        config.update({'transformer_layers': serialize_layers_collection(self.transformer_layers)})
+        return config
 
-# We don't need the common classification and regression heads for
-# the layerflow API, why? because in default API it makes sense to
-# have the common heads since they allow parameters values to customize
-# the structure and behavior for the given architecture,
-# but here, the common heads will have the exact same interface i.e.
-# the exact same two parameters as the architecture specific ones
-# NOT only does it not make sense to have them but ALSO there's
-# no way to customize these for the specific architectures.
-# The better solution is to use the architecture specific heads from
-# the default api as base for the architecture specific heads in the
-# layerflow api
-
+    @classmethod
+    def from_config(cls, config):
+        transformer_layers = deserialize_layers_collection(config.pop("transformer_layers"))
+        return cls(transformer_layers, **config)
