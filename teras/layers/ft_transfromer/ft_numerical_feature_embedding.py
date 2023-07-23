dc548f69f3efe355669acca078bae064bc5847a8
abaid <khawaja.abaid@gmail.com>
2023-07-23 10:45:34 +0500

    Put FTTransformer layers in a sub package, update names and remove old ft_transformer.py layers module

--- teras/layers/ft_transfromer/ft_numerical_feature_embedding.py
+++ teras/layers/ft_transfromer/ft_numerical_feature_embedding.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-class NumericalFeatureEmbedding(keras.layers.Layer):
+class FTNumericalFeatureEmbedding(keras.layers.Layer):
     """
-    Numerical Feature Embedding layer as proposed by
+    Numerical Feature Emebdding layer as proposed by
     Yury Gorishniy et al. in the paper,
     Revisiting Deep Learning Models for Tabular Data
     in their FTTransformer architecture.
@@ -94,51 +94,3 @@ class NumericalFeatureEmbedding(keras.layers.Layer):
     def from_config(cls, config):
         features_metadata = config.pop("features_metadata")
         return cls(features_metadata, **config)
-
-
-# TODO rework this layer -- ideally making it simple!
-class CLSToken(keras.layers.Layer):
-    """
-    CLS Token as proposed and implemented by Yury Gorishniy et al.
-    in the paper Revisiting Deep Learning Models for Tabular Data
-    in their FTTransformer architecture.
-
-    Reference(s):
-        https://arxiv.org/abs/2106.11959
-
-    Args:
-        embedding_dim: ``int``, default 32,
-            Embedding dimensions used to embed the numerical
-            and categorical features.
-
-        initialization: ``str``, default "normal",
-            Initialization method to use for the weights.
-    """
-    def __init__(self,
-                 embedding_dim: int = 32,
-                 initialization: str = "normal",
-                 **kwargs):
-        super().__init__(**kwargs)
-        self.embedding_dim = embedding_dim
-        self.initialization = initialization
-
-    def build(self, input_shape):
-        initializer = self.initialization
-        if isinstance(initializer, str):
-            initializer = keras.initializers.get(initializer)
-        self.weight = self.add_weight(initializer=initializer,
-                                      shape=(self.embedding_dim,))
-
-    def expand(self, *leading_dimensions: int) -> tf.Tensor:
-        if not leading_dimensions:
-            return self.weight
-        return tf.broadcast_to(tf.expand_dims(self.weight, axis=0), (*leading_dimensions, self.weight.shape[0]))
-
-    def call(self, inputs):
-        return tf.concat([inputs, self.expand(tf.shape(inputs)[0], 1)], axis=1)
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({'embedding_dim': self.embedding_dim,
-                      'initialization': self.initialization})
-        return config
