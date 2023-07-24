f1e70ca23faa207e8ecbd5ce7b1cb9cdc300769e
abaid <khawaja.abaid@gmail.com>
2023-07-24 22:42:57 +0500

    Redesign `NODE` models
    1. Now LayerFlow version serves as the parent for parametric
    
    2. Models are now constructed and behave like Functional instead of subclassed
    
    3. Update docstrings etc

--- teras/layerflow/models/node.py
+++ teras/layerflow/models/node.py
@@ -1,14 +1,14 @@
 from tensorflow import keras
-from tensorflow.keras import layers, models
-from teras.models import NODE as _BaseNODE
-from teras.layers import NODEClassificationHead, NODERegressionHead
 from typing import List
-from teras.utils import serialize_layers_collection
+from teras.utils.utils import (serialize_layers_collection,
+                               deserialize_layers_collection)
+from teras.layers.node.node_features_selector import NodeFeatureSelector
 
-LIST_OF_LAYERS = List[layers.Layer]
+LIST_OF_LAYERS = List[keras.layers.Layer]
 
 
-class NODE(_BaseNODE):
+@keras.saving.register_keras_serializable(package="teras.layerflow.models")
+class NODE(keras.Model):
     """
     Neural Oblivious Decision Tree (NODE) model with LayerFlow design.
     It is based on the NODE architecture proposed by Sergei Popov et al.
@@ -19,145 +19,84 @@ class NODE(_BaseNODE):
         https://arxiv.org/abs/1909.06312
 
     Args:
-        tree_layers: `List[layers.Layer]`,
+        input_dim: ``int``,
+            Dimensionality of the input dataset,
+            or, the number of features in the dataset.
+
+        tree_layers: ``List[layers.Layer]``,
             A list or tuple of `ObliviousDecisionTree` layers instances, or any custom
             layer that can work in its place.
             If None, default number of `ObliviousDecisionTree` layers with default values
             will be used.
             You can import the `ObliviousDecisionTree` layer as follows,
                 >>> from teras.layerflow.layers import ObliviousDecisionTree
-        head: `layers.Layer`,
-            An instance of `NODEClassificationHead` or `NODERegressionHead`
-            layer for final outputs,
-            or any layer that can work in place of a head layer for that purpose.
-        max_features: `int`, default None,
-            Maximum number of input features to use.
-            If None, all features in the input dataset will be used.
-        input_dropout: `float`, default 0.,
-            Dropout rate to apply to inputs.
+
+        feature_selector: ``keras.layers.Layer``:
+            An instance of ``NodeFeatureSelector`` layer that selects features based on
+            In None, all features will be used (no ``NodeFeatureSelector`` layer will be applied).
+            You can import the ``NodeFeatureSelector`` layer as follows,
+                >>> from teras.layers import NodeFeatureSelector
+
+        dropout: `keras.layers.Layer``,
+            An instance of ``Dropout`` layer to apply over inputs.
+            If None, no dropout will be applied.
+            You can import the ``Dropout`` layer as follows,
+                >>> from keras.layers import Dropout
+
+        head: ``keras.layers.Layer``,
+            An instance of ``Head`` layer to make classification or regression predictions.
+            In case you're using this model as a base model for pretraining, you MUST leave
+            this argument as None.
+            You can import the ``Head`` layer as follows,
+                >>> from teras.layerflow.layers import Head
+            or you can just use your own custom layer.
     """
     def __init__(self,
+                 input_dim: int,
                  tree_layers: LIST_OF_LAYERS = None,
-                 head: layers.Layer = None,
-                 max_features: int = None,
-                 input_dropout: float = 0.,
+                 feature_selector: keras.layers.Layer = None,
+                 dropout: keras.layers.Layer = None,
+                 head: keras.layers.Layer = None,
                  **kwargs):
-        super().__init__(max_features=max_features,
-                         input_dropout=input_dropout,
-                         **kwargs)
-        if tree_layers is not None:
-            if not isinstance(tree_layers, (list, tuple)):
-                raise ValueError("`tree_layers` must be a list or tuple of `ObliviousDecisionTree` layers, "
-                                 f"but recieved type: {type(tree_layers)}.")
-            self.tree_layers = tree_layers
+        if not isinstance(tree_layers, (list, tuple)):
+            raise ValueError("`tree_layers` must be a list or tuple of `ObliviousDecisionTree` layers, "
+                             f"but received type: {type(tree_layers)}.")
 
+        inputs = keras.layers.Input(shape=(input_dim,),
+                                    name="inputs")
+        x_out = inputs
+        initial_features = input_dim
+        for layer in tree_layers:
+            x = x_out
+            if feature_selector is not None:
+                x = feature_selector(x)
+            if dropout is not None:
+                x = dropout(x)
+            h = layer(x)
+            x_out = keras.layers.Concatenate(axis=-1)([x_out, h])
+        outputs = x_out[..., initial_features:]
         if head is not None:
-            self.head = head
+            outputs = head(outputs)
+        super().__init__(inputs=inputs,
+                         outputs=outputs,
+                         **kwargs)
+
+        self.tree_layers = tree_layers
+        self.head = head
 
     def get_config(self):
         config = super().get_config()
-        new_config = {'tree_layers': serialize_layers_collection(self.tree_layers),
-                      'head': keras.layers.serialize(self.head)
-                      }
-        config.update(new_config)
+        config.update({'tree_layers': serialize_layers_collection(self.tree_layers),
+                       'head': keras.layers.serialize(self.head),
+                       'max_features': self.max_features,
+                       'input_dropout': self.input_dropout
+                       })
         return config
 
-
-class NODEClassifier(NODE):
-    """
-    NODEClassifier with LayerFlow design.
-    It is based on the NODE architecture proposed by Sergei Popov et al.
-    in the paper,
-    Neural Oblivious Decision Ensembles for Deep Learning on Tabular Data
-
-    Reference(s):
-        https://arxiv.org/abs/1909.06312
-
-    Args:
-        tree_layers: `List[layers.Layer]`,
-            A list or tuple of `ObliviousDecisionTree` layers instances, or any custom
-            layer that can work in its place.
-            If None, default number of `ObliviousDecisionTree` layers with default values
-            will be used.
-            You can import the `ObliviousDecisionTree` layer as follows,
-                >>> from teras.layerflow.layers import ObliviousDecisionTree
-        head: `layers.Layer`,
-            An instance of `NODEClassificationHead` layer for final outputs,
-            or any layer that can work in place of a head layer for that purpose.
-            You can import the `NODEClassificationHead` as follows,
-                >>> from teras.layerflow.layers import NODEClassificationHead
-        max_features: `int`, default None,
-            Maximum number of input features to use.
-            If None, all features in the input dataset will be used.
-        input_dropout: `float`, default 0.,
-            Dropout rate to apply to inputs.
-    """
-    def __init__(self,
-                 tree_layers: LIST_OF_LAYERS = None,
-                 head: layers.Layer = None,
-                 max_features: int = None,
-                 input_dropout: float = 0.,
-                 **kwargs):
-        if head is None:
-            num_classes = 2
-            activation_out = None
-            if "num_classes" in kwargs:
-                num_classes = kwargs.pop("num_classes")
-            if "activation_out" in kwargs:
-                activation_out = kwargs.pop("activation_out")
-            head = NODEClassificationHead(num_classes=num_classes,
-                                          activation_out=activation_out,
-                                          name="node_classification_head")
-        super().__init__(tree_layers=tree_layers,
-                         head=head,
-                         max_features=max_features,
-                         input_dropout=input_dropout,
-                         **kwargs)
-
-
-class NODERegressor(NODE):
-    """
-    NODERegressor model with LayerFlow design.
-    It is based on the NODE architecture proposed by Sergei Popov et al.
-    in the paper,
-    Neural Oblivious Decision Ensembles for Deep Learning on Tabular Data
-
-    Reference(s):
-        https://arxiv.org/abs/1909.06312
-
-    Args:
-        tree_layers: `List[layers.Layer]`,
-            A list or tuple of `ObliviousDecisionTree` layers instances, or any custom
-            layer that can work in its place.
-            If None, default number of `ObliviousDecisionTree` layers with default values
-            will be used.
-            You can import the `ObliviousDecisionTree` layer as follows,
-                >>> from teras.layerflow.layers import ObliviousDecisionTree
-        head: `layers.Layer`,
-            An instance of `NODERegressionHead` layer for final outputs,
-            or any layer that can work in place of a head layer for that purpose.
-            You can import the `NODERegressionHead` as follows,
-                >>> from teras.layerflow.layers import NODERegressionHead
-        max_features: `int`, default None,
-            Maximum number of input features to use.
-            If None, all features in the input dataset will be used.
-        input_dropout: `float`, default 0.,
-            Dropout rate to apply to inputs.
-    """
-    def __init__(self,
-                 tree_layers: LIST_OF_LAYERS = None,
-                 head: layers.Layer = None,
-                 max_features: int = None,
-                 input_dropout: float = 0.,
-                 **kwargs):
-        if head is None:
-            num_outputs = 1
-            if "num_outputs" in kwargs:
-                num_outputs = kwargs.pop("num_outputs")
-            head = NODERegressionHead(num_outputs=num_outputs,
-                                      name="node_regression_head")
-        super().__init__(tree_layers=tree_layers,
-                         head=head,
-                         max_features=max_features,
-                         input_dropout=input_dropout,
-                         **kwargs)
+    @classmethod
+    def from_config(cls, config):
+        tree_layers = deserialize_layers_collection(config.pop("tree_layers"))
+        head = keras.layers.deserialize(config.pop("head"))
+        return cls(tree_layers=tree_layers,
+                   head=head,
+                   **config)
