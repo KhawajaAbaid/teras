00626cb6802fb7bff8becd3bd477ccfbf5f27895
abaid <khawaja.abaid@gmail.com>
2023-07-24 15:10:16 +0500

    Add layer imports from modules to __init__

--- teras/layerflow/layers/common/__init__.py
+++ teras/layerflow/layers/common/__init__.py
@@ -11,3 +11,16 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+# General purpose common layers
+from teras.layerflow.layers.common.common import HiLOL
+
+
+# Transformer layers
+from teras.layerflow.layers.common.transformer import (FeedForward,
+                                                       Transformer,
+                                                       Encoder)
+
+# Head layers
+from teras.layerflow.layers.common.head import Head
