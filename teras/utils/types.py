2f152610d21ca6d69290375d6178bd2af7872ec7
abaid <khawaja.abaid@gmail.com>
2023-07-24 11:48:22 +0500

    Add `types` module to teras/utils

---
+++ teras/utils/types.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from typing import Union, List
+import numpy as np
+
+
+# Taken from TensorFlow Addons
+
+Number = Union[
+                float,
+                int,
+                np.float16,
+                np.float32,
+                np.float64,
+                np.int8,
+                np.int16,
+                np.int32,
+                np.int64,
+                np.uint8,
+                np.uint16,
+                np.uint32,
+                np.uint64,
+            ]
+
+
+TensorLike = Union[
+                List[Union[Number, list]],
+                tuple,
+                Number,
+                np.ndarray,
+                tf.Tensor,
+                tf.SparseTensor,
+                tf.Variable,
+            ]
