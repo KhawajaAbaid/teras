7efa210094333f172a885bd48bd92e6a349f2cb6
abaid <khawaja.abaid@gmail.com>
2023-07-26 14:50:24 +0500

    Redesign TabNet models
    1. Now LayerFlow version models serve as parent
    
    2. Remove support for dictionary type datasets
    
    3. Update docstrings and stuff
    
    4. Remove Classifier and Regressor model classes from LayerFlow version

--- teras/layerflow/models/tabnet.py
+++ teras/layerflow/models/tabnet.py
@@ -1,5 +1,9 @@
+import tensorflow as tf
 from tensorflow import keras
-from teras.layers.common.head import ClassificationHead, RegressionHead
+from tensorflow.keras import backend as K
+from teras.layers.numerical_features_extractor import NumericalFeaturesExtractor
+from teras.losses.tabnet import reconstruction_loss
+import tensorflow_probability as tfp
 
 
 class TabNet(keras.Model):
@@ -19,13 +23,17 @@ class TabNet(keras.Model):
         https://arxiv.org/abs/1908.07442
 
     Args:
+        input_dim: ``int``,
+            Dimensionality of the input dataset,
+            or the number of features in the input dataset.
+
         features_metadata: ``dict``,
-            a nested dictionary of metadata for features where
+            A nested dictionary of metadata for features where
             categorical sub-dictionary is a mapping of categorical feature names to a tuple of
             feature indices and the lists of unique values (vocabulary) in them,
             while numerical dictionary is a mapping of numerical feature names to their indices.
-            `{feature_name: (feature_idx, vocabulary)}` for feature in categorical features.
-            `{feature_name: feature_idx}` for feature in numerical features.
+            ``{feature_name: (feature_idx, vocabulary)}`` for feature in categorical features.
+            ``{feature_name: feature_idx}`` for feature in numerical features.
             You can get this dictionary from
                 >>> from teras.utils import get_features_metadata_for_embedding
                 >>> metadata_dict = get_features_metadata_for_embedding(dataframe,
@@ -33,11 +41,26 @@ class TabNet(keras.Model):
                 ..                                                      numerical_features)
 
         categorical_feature_embedding: ``keras.layers.Layer``,
-            An instance of `CategoricalFeatureEmbedding` layer to embedd categorical features
-            or any layer that can work in place of `CategoricalFeatureEmbedding` for that purpose.
-            If None, a `CategoricalFeatureEmbedding` layer with default values will be used.
-            You can import the `CategoricalFeatureEmbedding` layer as follows,
+            An instance of ``CategoricalFeatureEmbedding`` layer to embedd categorical features
+            or any layer that can work in its place.
+            You can import the ``CategoricalFeatureEmbedding`` layer as follows,
                 >>> from teras.layers import CategoricalFeatureEmbedding
+
+        numerical_feature_embedding: ``keras.layers.Layer``,
+            The ``TabNet`` archietcture does not apply any embedding layer to numerical features
+            by default, for the sake of modularity, ``Teras`` allows you to use any numerical
+            feature embedding layer if you want to.
+
+            NOTE: if you do use numerical feature embedding layer, its dimensionality MUST
+            be equal to the dimensionality of the categorical feature embedding.
+
+            Currently, Teras offers two different numerical feature embedding layers that you can
+            import as follows,
+                1. ``SAINTNumericalFeatureEmbedding`` layer proposed by ``SAINT`` architecture,
+                    >>> from teras.layers import SAINTNumericalFeatureEmbedding
+
+                2. ``FTNumericalFeatureEmebdding`` layer proposed by ``FTTransformer`` architecure,
+                    >>> from teras.layers import FTNumericalFeatureEmbedding
 
         encoder: ``keras.layers.Layer``,
             An instance of Encoder layer to encode feature embeddings,
@@ -57,30 +80,78 @@ class TabNet(keras.Model):
                 >>> from teras.layers import RegressionHead
     """
     def __init__(self,
+                 input_dim: int,
                  features_metadata: dict,
-                 categorical_features_embedding: keras.layers.Layer = None,
+                 categorical_feature_embedding: keras.layers.Layer = None,
+                 numerical_feature_embedding: keras.layers.Layer = None,
                  encoder: keras.layers.Layer = None,
                  head: keras.layers.Layer = None,
                  **kwargs):
-        super().__init__(features_metadata=features_metadata,
-                         **kwargs)
-        if categorical_features_embedding is not None:
-            self.categorical_features_embedding = categorical_features_embedding
+        numerical_features_exist = len(features_metadata["numerical"]) > 0
+        inputs = keras.layers.Input(shape=(input_dim,))
+        x = inputs
+        if encoder is None:
+            raise ValueError("`encoder` cannot be None. Please pass an instance of `TabNetEncoder` layer. "
+                             "You can import it as, `from teras.layers import TabNetEncoder``")
 
-        if encoder is not None:
-            self.encoder = encoder
+        categorical_embeddings = None
+        if categorical_feature_embedding is not None:
+            categorical_embeddings = categorical_feature_embedding(inputs)
+            categorical_embeddings = keras.layers.Flatten()(categorical_embeddings)
 
+        if numerical_feature_embedding is not None:
+            numerical_embeddings = numerical_feature_embedding(inputs)
+            numerical_embeddings = keras.layers.Flatten()(numerical_embeddings)
+            if categorical_embeddings is not None:
+                x = keras.layers.Concatenate(axis=1)([categorical_embeddings, numerical_embeddings])
+        else:
+            if numerical_features_exist:
+                # If user doesn't want to apply any numerical embedding, but numerical
+                # features do exist, we extract them as is and concatenate them with
+                # categorical feature embeddings which have been flattened -- hence allowing the concatenation
+                numerical_features = NumericalFeaturesExtractor(features_metadata)(inputs)
+                x = keras.layers.Concatenate(axis=1)([categorical_embeddings, numerical_features])
+        outputs = encoder(x)
         if head is not None:
-            self.head = head
+            outputs = head(outputs)
+        super().__init__(inputs=inputs,
+                         outputs=outputs,
+                         **kwargs)
+
+        self.input_dim = input_dim
+        self.features_metadata = features_metadata
+        self.categorical_feature_embedding = categorical_feature_embedding
+        self.numerical_feature_embedding = numerical_feature_embedding
+        self.encoder = encoder
+        self.head = head
 
     def get_config(self):
         config = super().get_config()
-        new_config = {'categorical_features_embedding': keras.layers.serialize(self.categorical_features_embedding),
-                      'encoder': keras.layers.serialize(self.encoder),
-                      'head': keras.layers.serialize(self.head),
-                      }
-        config.update(new_config)
+        config.update({'input_dim': self.input_dim,
+                       'features_metadata': self.features_metadata,
+                       'categorical_features_embedding': keras.layers.serialize(self.categorical_features_embedding),
+                       'numerical_feature_embedding': keras.layers.serialize(self.numerical_feature_embedding),
+                       'encoder': keras.layers.serialize(self.encoder),
+                       'head': keras.layers.serialize(self.head),
+                       }
+                      )
         return config
+
+    @classmethod
+    def from_config(cls, config):
+        input_dim = config.pop("input_dim")
+        features_metadata = config.pop("features_metadata")
+        categorical_features_embedding = keras.layers.deserialize(config.pop("categorical_features_embedding"))
+        numerical_feature_embedding = keras.layers.deserialize(config.pop("numerical_feature_embedding"))
+        encoder = keras.layers.deserialize(config.pop("encoder"))
+        head = keras.layers.deserialize(config.pop("head"))
+        return cls(input_dim=input_dim,
+                   features_metadata=features_metadata,
+                   categorical_features_embedding=categorical_features_embedding,
+                   numerical_feature_embedding=numerical_feature_embedding,
+                   encoder=encoder,
+                   head=head,
+                   **config)
 
 
 class TabNetPretrainer(keras.Model):
@@ -100,14 +171,14 @@ class TabNetPretrainer(keras.Model):
         model: ``TabNet``,
             An instance of base ``TabNet`` model to pretrain.
 
-        decoder: `keras.layers.Layer`,
+        decoder: ``keras.layers.Layer``,
             An instance of ``TabNetDecoder`` layer or any custom layer
             that can be used in its place to reconstruct the input
             features from the encoded representations.
             You can import the ``TabNetDecoder`` layer as
                 >>> from teras.layers import TabNetDecoder
 
-        missing_feature_probability: ``float``, default 3,
+        missing_feature_probability: ``float``, default 0.3,
             Fraction of features to randomly mask i.e. make them missing.
             Missing features are introduced in the pretraining dataset and
             the probability of missing features is controlled by the parameter.
@@ -116,20 +187,134 @@ class TabNetPretrainer(keras.Model):
     """
     def __init__(self,
                  model: TabNet,
+                 features_metadata: dict,
                  decoder: keras.layers.Layer = None,
                  missing_feature_probability: float = 0.3,
                  **kwargs):
-        super().__init__(model=model,
-                         missing_feature_probability=missing_feature_probability,
-                         **kwargs)
+        super().__init__(**kwargs)
+        self.model = model
+        self.features_metadata = features_metadata
+        self.decoder = decoder
+        self.missing_feature_probability = missing_feature_probability
 
-        if decoder is not None:
-            self.decoder = decoder
+        self.numerical_features_indices = list(self.features_metadata["numerical"].values())
+        self._categorical_features_exist = len(self.features_metadata["categorical"]) > 0
+        self._numerical_features_exist = len(self.features_metadata["numerical"]) > 0
+        self.input_dim = len(self.features_metadata["categorical"]) + len(self.features_metadata["numerical"])
+
+        self.binary_mask_generator = tfp.distributions.Binomial(total_count=1,
+                                                                probs=self.missing_feature_probability,
+                                                                name="binary_mask_generator")
+        self._reconstruction_loss_tracker = keras.metrics.Mean(name="reconstruction_loss")
+
+    def get_pretrained_model(self):
+        """Returns pretrained model"""
+        return self.model
+
+    @property
+    def pretrained_model(self):
+        """Returns pretrained model"""
+        return self.model
+
+    @property
+    def feature_importances_per_sample(self):
+        """Returns feature importances per sample computed during training."""
+        return tf.concat(self.encoder.feature_importances_per_sample, axis=0)
+
+    @property
+    def feature_importances(self):
+        """Returns average feature importances across samples computed during training."""
+        return tf.reduce_mean(self.feature_importances_per_sample, axis=0)
+
+    def compile(self,
+                loss=reconstruction_loss,
+                optimizer=keras.optimizers.Adam(learning_rate=0.001),
+                **kwargs):
+        super().compile(**kwargs)
+        self.reconstruction_loss = loss
+        self.optimizer = optimizer
+
+    def call(self, inputs, mask=None):
+        # this mask below is what `S` means in the paper, where if an index contains
+        # value 1, it means that it is missing
+        encoder_input = (1 - mask) * inputs
+        # The paper says,
+        # The TabNet encoder inputs (1 − S) · f, where f is the original features
+        # and the decoder outputs the reconstructed features, S · f^, where f^ is the reconstructed features
+        # We initialize P[0] = (1 − S) in encoder so that the model emphasizes merely on the known features.
+        # -- So we pass the mask from here, the encoder checks if it received a value for mask, if so it won't
+        # initialized the `mask_values` variable in its call method to zeros.
+        encoded_representations = self.model.encoder(encoder_input, mask=(1 - mask))
+        decoder_outputs = self.decoder(encoded_representations)
+        return decoder_outputs
+
+    def train_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+        cat_embed_dim = 0
+        num_embed_dim = 0
+
+        with tf.GradientTape() as tape:
+            embedded_inputs = None
+            if self._categorical_features_exist:
+                categorical_embeddings = self.model.categorical_feature_embedding(data)
+                categorical_embeddings = K.flatten(categorical_embeddings)
+                cat_embed_dim = tf.shape(categorical_embeddings)[1]
+                embedded_inputs = categorical_embeddings
+            # We need to concatenate numerical features to the categorical embeddings
+            if self._numerical_features_exist:
+                if (hasattr(self.model, "numerical_feature_embedding")
+                        and self.model.numerical_feature_embedding is not None):
+                    numerical_embeddings = self.model.numerical_feature_embedding(data)
+                else:
+                    numerical_embeddings = tf.gather(data,
+                                                     indices=self.numerical_features_indices,
+                                                     axis=1)
+                num_embed_dim = tf.shape(numerical_embeddings)[1]
+                if embedded_inputs is None:
+                    embedded_inputs = numerical_embeddings
+                else:
+                    embedded_inputs = K.concatenate([embedded_inputs, numerical_embeddings], axis=1)
+            total_dim = cat_embed_dim + num_embed_dim
+            embedded_inputs.set_shape((None, total_dim))
+            # Generate mask to create missing samples
+            batch_size = tf.shape(embedded_inputs)[0]
+            mask = self.binary_mask_generator.sample(sample_shape=(batch_size, total_dim))
+            tape.watch(mask)
+            # Reconstruct samples
+            reconstructed_samples = self(embedded_inputs, mask=mask)
+            # Compute reconstruction loss
+            loss = self.reconstruction_loss(real_samples=embedded_inputs,
+                                            reconstructed_samples=reconstructed_samples,
+                                            mask=mask)
+        gradients = tape.gradient(loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_weights))
+        self._reconstruction_loss_tracker.update_state(loss)
+        # If user has passed any additional metrics to compile, we should update their states
+        if len(self.compiled_metrics.metrics) > 0:
+            self.compiled_metrics.update_state(embedded_inputs, reconstructed_samples)
+        # If user has passed any additional losses to compile, we should call them
+        if self.compiled_loss._losses is not None:
+            self.compiled_loss(embedded_inputs, reconstructed_samples)
+        results = {m.name: m.result() for m in self.metrics}
+        return results
 
     def get_config(self):
         config = super().get_config()
-        new_config = {'model': keras.layers.serialize(self.model),
-                      'decoder': keras.layers.serialize(self.decoder)
-                      }
-        config.update(new_config)
+        config.update({'model': keras.layers.serialize(self.model),
+                       'features_metadata': self.features_metadata,
+                       'decoder': keras.layers.serialize(self.decoder),
+                       'missing_feature_probability': self.missing_feature_probability
+                       }
+                      )
         return config
+
+    @classmethod
+    def from_config(cls, config):
+        model = keras.layers.deserialize(config.pop("model"))
+        features_metadata = config.pop("features_metadata")
+        decoder = keras.layers.deserialize(config.pop("decoder"))
+        return cls(model=model,
+                   features_metadata=features_metadata,
+                   decoder=decoder,
+                   **config)
