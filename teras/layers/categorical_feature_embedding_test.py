98aff9443253fafc9ba1feb7721853bab88b6ca9
abaid <khawaja.abaid@gmail.com>
2023-07-26 16:38:08 +0500

    Rename `embedding_test` to `categorical_feature_embedding_test.py` and update units tests

--- teras/layers/categorical_feature_embedding_test.py
+++ teras/layers/categorical_feature_embedding_test.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from teras.layers.embedding import CategoricalFeatureEmbedding
+from teras.layers.categorical_feature_embedding import CategoricalFeatureEmbedding
 from teras.utils import get_features_metadata_for_embedding
 import pandas as pd
 import pytest
@@ -31,3 +31,13 @@ def test_categorical_feature_embedding_output_shape(setup_data):
     assert tf.shape(outputs)[0] == 8    # number of items in each column
     assert tf.shape(outputs)[1] == 2
     assert tf.shape(outputs)[2] == 32
+
+
+def test_categorical_feature_embedding_raises_error_when_categorical_features_dont_exist():
+    df = pd.DataFrame({'income': [1000, 1270, 900, 1000, 1234, 5678, 9999, 0],
+                       'goals': [900, 70, 222, 131, 1337, 99, 0, 656]})
+    metadata = get_features_metadata_for_embedding(df,
+                                                   categorical_features=None,
+                                                   numerical_features=['income', 'goals'])
+    with pytest.raises(ValueError):
+        extractor = CategoricalFeatureEmbedding(features_metadata=metadata)
