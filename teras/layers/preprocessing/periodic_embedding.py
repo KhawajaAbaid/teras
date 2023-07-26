39ecd464d7c0b5a6d1d60db19cd82925361a2512
abaid <khawaja.abaid@gmail.com>
2023-07-26 15:55:42 +0500

    Update `PeriodicEmbedding` layer

--- teras/layers/preprocessing/periodic_embedding.py
+++ teras/layers/preprocessing/periodic_embedding.py
@@ -1,13 +1,14 @@
-from tensorflow.keras import layers
 import tensorflow as tf
+from tensorflow import keras
 from typing import Literal
 import math
 
-PERIOD_INITIALIZATIONS = Literal['log-linear', 'normal']
+PERIOD_INITIALIZATIONS = Literal["log-linear", "normal"]
 
 
-class PeriodicEmbedding(layers.Layer):
-    """Period embedding layer for numerical features
+class PeriodicEmbedding(keras.layers.Layer):
+    """
+    PeriodicEmbedding layer for numerical features
     as proposed Yury Gorishniy et al. in the paper
     On Embeddings for Numerical Features in Tabular Deep Learning, 2022.
 
@@ -15,41 +16,47 @@ class PeriodicEmbedding(layers.Layer):
         https://arxiv.org/abs/2203.05556
 
     Args:
-        num_features: `int`,
-            Number of features in the dataset.
-        embedding_dim: `int`, default 32,
+        data_dim: ``int``,
+            Dimensionality of the input dataset,
+            or the number of features in the dataset.
+
+        embedding_dim: ``int``, default 32,
             Dimensionality of numerical embeddings
+
         initialization: default "normal",
             Initialization strategy.
-        sigma: `float`, default 0.01
+
+        sigma: ``float``, default 0.01,
             Used for coefficients initialization
     """
     def __init__(self,
-                 num_features: int,
+                 data_dim: int,
                  embedding_dim: int = 32,
-                 initialization: PERIOD_INITIALIZATIONS = 'normal',
+                 initialization: PERIOD_INITIALIZATIONS = "normal",
                  sigma: float = 0.01,
                  **kwargs):
         super().__init__(**kwargs)
-        assert initialization.lower() in ['normal', 'log-linear'], ("Invalid value for initialization."
-                                                                    " Must be one of ['log-linear', 'normal']")
+        if initialization.lower() not in ["normal", "log-linear"]:
+            raise ValueError("Invalid value for initialization. Must be one of ['log-linear', 'normal']\n"
+                             f"Received: {initialization}")
         self.embedding_dim = embedding_dim
-        self.num_features = num_features
+        self.data_dim = data_dim
         self.initialization = initialization.lower()
         self.sigma = sigma
 
         # The official implementation uses another variable n, that is half of the embedding dim
         self.n = self.embedding_dim // 2
+        self.coefficients = None
 
     def build(self, input_shape):
         if self.initialization == 'log-linear':
             self.coefficients = self.sigma ** (tf.range(self.n) / self.n)
             self.coefficients = tf.repeat(self.coefficients[None],
-                                          repeats=self.num_features,
+                                          repeats=self.data_dim,
                                           axis=1)
         else:
             # initialization must be normal
-            self.coefficients = tf.random.normal(shape=(self.num_features, self.n),
+            self.coefficients = tf.random.normal(shape=(self.data_dim, self.n),
                                                  mean=0.,
                                                  stddev=self.sigma)
 
@@ -65,11 +72,15 @@ class PeriodicEmbedding(layers.Layer):
 
     def get_config(self):
         config = super().get_config()
-        new_config = {'num_features': self.num_features,
-                      'embedding_dim': self.embedding_dim,
-                      'initialization': self.initialization,
-                      'sigma': self.sigma,
-                      }
-
-        config.update(new_config)
+        config.update({'data_dim': self.data_dim,
+                       'embedding_dim': self.embedding_dim,
+                       'initialization': self.initialization,
+                       'sigma': self.sigma,
+                       })
         return config
+
+    @classmethod
+    def from_config(cls, config):
+        data_dim = config.pop("data_dim")
+        return cls(data_dim,
+                   **config)
