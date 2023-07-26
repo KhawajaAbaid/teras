b0a7dd458839f7022021730df4f92db890c2a665
abaid <khawaja.abaid@gmail.com>
2023-07-26 22:00:01 +0500

    Implement unit tests for `LabelEncoding` layer

--- teras/layers/preprocessing/label_encoding_test.py
+++ teras/layers/preprocessing/label_encoding_test.py
@@ -1,15 +1,17 @@
 import tensorflow as tf
-from teras.layers.encoding import LabelEncoding
+from teras.layers.preprocessing.label_encoding import LabelEncoding
 from teras.utils import get_features_metadata_for_embedding
 import pandas as pd
+import numpy as np
 import pytest
 
 
 @pytest.fixture
 def setup_data():
     df = pd.DataFrame({'player_level': [70, 90, 80, 90, 100, 90, 70, 80],
-            'shirt_number': [10, 7, 7, 10, 10, 10, 7, 10],
-            'income': [1000, 1270, 900, 1000, 1234, 5678, 9999, 0]})
+                       'income': [1000, 1270, 900, 1000, 1234, 5678, 9999, 0],
+                       'shirt_number': [10, 7, 7, 10, 10, 10, 7, 10],
+                       })
     metadata = get_features_metadata_for_embedding(df,
                                                    categorical_features=["player_level", "shirt_number"],
                                                    numerical_features=["income"])
@@ -35,3 +37,14 @@ def test_label_encoding_output_shape_when_concatenate_numerical_features_is_true
     assert tf.shape(outputs)[0] == 8
     assert tf.shape(outputs)[1] == 3    # total number of features = 3
 
+
+def test_label_encoding_keeps_features_order(setup_data):
+    data, metadata = setup_data
+    label_encoding = LabelEncoding(features_metadata=metadata,
+                                   concatenate_numerical_features=True,
+                                   keep_features_order=True)
+    outputs = label_encoding(data)
+    assert len(tf.shape(outputs)) == 2
+    assert tf.shape(outputs)[0] == 8
+    assert tf.shape(outputs)[1] == 3    # total number of features = 3
+    assert all(data[:, 1] == outputs[:, 1])
