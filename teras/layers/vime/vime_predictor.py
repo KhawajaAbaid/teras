3486d2b432abc0897b98303c09bee360d82d1669
abaid <khawaja.abaid@gmail.com>
2023-07-27 20:06:24 +0500

    Update `VimePredictor`:
    1. Remove the need for `data_dim`
    
    2. Remove `from_config` method

--- teras/layers/vime/vime_predictor.py
+++ teras/layers/vime/vime_predictor.py
@@ -13,14 +13,10 @@ class VimePredictor(keras.layers.Layer):
         https://proceedings.neurips.cc/paper/2020/hash/7d97667a3e056acab9aaf653807b4a03-Abstract.html
 
     Args:
-        data_dim: ``int``,
-            The dimensionality of the input dataset,
-            or the number of features in the dataset.
-
         num_labels: `int`, default 32,
             Number of labels to predict
 
-        units: ``int``, default 32,
+        hidden_dim: ``int``, default 32,
             The hidden dimensionality of the predictor.
 
         activation: ``str`` or ``callable`` or ``keras.layers.Layer``, default "relu",
@@ -30,23 +26,18 @@ class VimePredictor(keras.layers.Layer):
             Batch size being used.
     """
     def __init__(self,
-                 data_dim: int = None,
                  num_labels: int = 2,
-                 units: int = 32,
+                 hidden_dim: int = 32,
                  activation: ActivationType = "relu",
                  batch_size: int = 512,
                  **kwargs):
         super().__init__(**kwargs)
-        self.data_dim = data_dim
         self.num_labels = num_labels
-        self.units = units
+        self.hidden_dim = hidden_dim
         self.activation = activation
         self.batch_size = batch_size
 
         self.predictor_block = keras.models.Sequential(name="predictor_block")
-        self.input_layer = keras.layers.Input(shape=(None, data_dim,),
-                                              batch_size=self.batch_size)
-        self.predictor_block.add(self.input_layer)
         self.inter_layer_1 = keras.layers.Dense(self.hidden_dim,
                                                 activation=self.activation,
                                                 name="inter_layer_1")
@@ -71,17 +62,10 @@ class VimePredictor(keras.layers.Layer):
         activation_serialized = self.activation
         if not isinstance(self.activation, str):
             activation_serialized = keras.layers.serialize(self.activation)
-        config.update({'data_dim': self.data_dim,
-                       'units': self.units,
-                       'num_labels': self.num_labels,
+        config.update({'num_labels': self.num_labels,
+                       'hidden_dim': self.hidden_dim,
                        'activation': activation_serialized,
                        'batch_size': self.batch_size,
                        }
                       )
         return config
-
-    @classmethod
-    def from_config(cls, config):
-        data_dim = config.pop("data_dim")
-        return cls(data_dim=data_dim,
-                   **config)
