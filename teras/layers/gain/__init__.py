11fe7261e4510d38076c3143c5d0b3e1226b3714
abaid <khawaja.abaid@gmail.com>
2023-07-27 16:56:30 +0500

    Add imports for `GAIN` layers at package level

--- teras/layers/gain/__init__.py
+++ teras/layers/gain/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+from teras.layers.gain.gain_generator_block import GAINGeneratorBlock
+from teras.layers.gain.gain_discriminator_block import GAINDiscriminatorBlock
