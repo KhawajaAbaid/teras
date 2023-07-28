d8c8da68aed445f22b96d29804c82da010b61374
abaid <khawaja.abaid@gmail.com>
2023-07-28 14:58:24 +0500

    Redesign CTGAN models
    1. Update LayerFlow version of CTGAN to serve as the parent to the parametric version
    
    2. Update docstrings, configs and imports

--- teras/models/ctgan.py
+++ teras/models/ctgan.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 from tensorflow import keras
-from teras.layers.ctgan import CTGANDiscriminatorBlock, CTGANGeneratorBlock
+from teras.layers.ctgan.ctgan_generator_block import CTGANGeneratorBlock
+from teras.layers.ctgan.ctgan_discriminator_block import CTGANDiscriminatorBlock
+from teras.layerflow.models.ctgan import CTGAN as _CTGAN_LF
 from teras.layers.activation import GumbelSoftmax
-from teras.losses.ctgan import generator_loss, discriminator_loss
-from tqdm import tqdm
 from teras.utils.types import UnitsValuesType
 
 
 @keras.saving.register_keras_serializable(package="teras.models")
-class CTGANGenerator(keras.Model):
+class CTGANGenerator(_CTGAN_LF):
     """
     CTGANGenerator for CTGAN architecture as proposed by
     Lei Xu et al. in the paper,
@@ -375,11 +375,22 @@ class CTGAN(keras.Model):
                  packing_degree: int = 8,
                  gradient_penalty_lambda: float = 10,
                  **kwargs):
-        super().__init__(**kwargs)
         if metadata is None:
             raise ValueError("`metadata` cannot be None. "
                              "You can access the `metadata` through `.get_metadata()` method of `CTGANDataTransformer` "
                              "instance.")
+        generator = CTGANGenerator(data_dim=input_dim,
+                                   metadata=metadata,
+                                   units_values=generator_units_values)
+
+        discriminator = CTGANDiscriminator(units_values=discriminator_units_values,
+                                           packing_degree=packing_degree,
+                                           gradient_penalty_lambda=gradient_penalty_lambda)
+        super().__init__(generator=generator,
+                         discriminator=discriminator,
+                         num_discriminator_steps=num_discriminator_steps,
+                         latent_dim=latent_dim,
+                         **kwargs)
         self.input_dim = input_dim
         self.metadata = metadata
         self.generator_units_values = generator_units_values
@@ -389,167 +400,18 @@ class CTGAN(keras.Model):
         self.packing_degree = packing_degree
         self.gradient_penalty_lambda = gradient_penalty_lambda
 
-        self.generator = CTGANGenerator(data_dim=self.input_dim,
-                                        metadata=self.metadata,
-                                        units_values=self.generator_units_values)
-
-        self.discriminator = CTGANDiscriminator(units_values=self.discriminator_units_values,
-                                                packing_degree=self.packing_degree,
-                                                gradient_penalty_lambda=self.gradient_penalty_lambda)
-
-        # Loss trackers
-        self.generator_loss_tracker = keras.metrics.Mean(name="generator_loss")
-        self.discriminator_loss_tracker = keras.metrics.Mean(name="discriminator_loss")
-
-    def compile(self,
-                generator_optimizer=keras.optimizers.Adam(learning_rate=1e-3,
-                                                          beta_1=0.5, beta_2=0.9),
-                discriminator_optimizer=keras.optimizers.Adam(learning_rate=1e-3,
-                                                              beta_1=0.5, beta_2=0.9),
-                generator_loss=generator_loss,
-                discriminator_loss=discriminator_loss,
-                ):
-        super().compile()
-        self.generator_optimizer = generator_optimizer
-        self.discriminator_optimizer = discriminator_optimizer
-        self.generator_loss = generator_loss
-        self.discriminator_loss = discriminator_loss
-
-    def call(self, inputs):
-        generated_samples = self.generator(inputs)
-        return generated_samples
-
-    def train_step(self, data):
-        # real_samples, shuffled_idx, random_features_indices, random_values_indices = data
-        real_samples, cond_vectors_real, cond_vectors, mask = data
-        self.batch_size = tf.shape(real_samples)[0]
-
-        for _ in range(self.num_discriminator_steps):
-            z = tf.random.normal(shape=[self.batch_size, self.latent_dim])
-            # cond_vector, mask = self.data_sampler.sample_cond_vector_for_training(self.batch_size,
-            #                                                                       random_features_indices=random_features_indices,
-            #                                                                       random_values_indices=random_values_indices)
-            input_gen = tf.concat([z, cond_vectors], axis=1)
-            generated_samples = self.generator(input_gen, training=False)
-            # cond_vector_2 = tf.gather(cond_vector, indices=tf.cast(shuffled_idx, tf.int32))
-            generated_samples = tf.concat([generated_samples, cond_vectors], axis=1)
-            real_samples = tf.concat([real_samples, cond_vectors_real], axis=1)
-
-            with tf.GradientTape(persistent=True) as tape:
-                y_generated = self.discriminator(generated_samples)
-                y_real = self.discriminator(real_samples)
-                grad_pen = self.discriminator.gradient_penalty(real_samples, generated_samples)
-                loss_disc = self.discriminator_loss(y_real, y_generated)
-            gradients_pen = tape.gradient(grad_pen, self.discriminator.trainable_weights)
-            gradients_loss = tape.gradient(loss_disc, self.discriminator.trainable_weights)
-            self.discriminator_optimizer.apply_gradients(zip(gradients_pen, self.discriminator.trainable_weights))
-            self.discriminator_optimizer.apply_gradients(zip(gradients_loss, self.discriminator.trainable_weights))
-
-        z = tf.random.normal(shape=[self.batch_size, self.latent_dim])
-        # cond_vector, mask = self.data_sampler.sample_cond_vector_for_training(self.batch_size,
-        #                                                                       random_features_indices=random_features_indices,
-        #                                                                       random_values_indices=random_values_indices)
-        # Practically speaking, we don't really need the partial function,
-        # but it makes things look more neat
-        # generator_partial_loss_fn = partial(generator_loss, mask=mask, metadata=self.metadata)
-        input_gen = tf.concat([z, cond_vectors], axis=1)
-        with tf.GradientTape() as tape:
-            tape.watch(cond_vectors)
-            tape.watch(mask)
-            # generated_samples, y_generated = self(input_gen, cond_vector=cond_vector)
-            generated_samples = self(input_gen)
-            generated_samples = tf.concat([generated_samples, cond_vectors], axis=1)
-            y_generated = self.discriminator(generated_samples, training=False)
-            loss_gen = self.generator_loss(generated_samples, y_generated,
-                                           cond_vectors=cond_vectors, mask=mask,
-                                           metadata=self.metadata)
-            # dummy_targets = tf.zeros(shape=(self.batch_size,))
-            # loss_gen_dummy = self.generator.compiled_loss(dummy_targets, loss_gen)
-        gradients = tape.gradient(loss_gen, self.generator.trainable_weights)
-        self.generator_optimizer.apply_gradients(zip(gradients, self.generator.trainable_weights))
-
-        self.generator_loss_tracker.update_state(loss_gen)
-        self.discriminator_loss_tracker.update_state(loss_disc)
-
-        results = {m.name: m.result() for m in self.metrics}
-        generator_results = {'generator_' + m.name: m.result() for m in self.generator.metrics}
-        results.update(generator_results)
-        discriminator_results = {'discriminator_' + m.name: m.result() for m in self.discriminator.metrics}
-        results.update(discriminator_results)
-        return results
-
-    def generate(self,
-                 num_samples: int,
-                 data_sampler,
-                 data_transformer=None,
-                 reverse_transform: bool = False,
-                 batch_size: int = None):
-        """
-        Generates new samples using the trained generator.
-
-        Args:
-            num_samples: ``int``,
-                Number of new samples to generate
-
-            data_sampler:
-                Instance of the ``CTGANDataSampler`` class used in preparing
-                the tensorflow dataset for training.
-
-            data_transformer:
-                Instance of ``CTGANDataTransformer`` class that was used to preprocess the raw data.
-                This is required only if the ``reverse_transform`` is set to True.
-
-            reverse_transform: ``bool``, default False,
-                Whether to reverse transform the generated data to the original data format.
-                If False, the raw generated data will be returned, which you can then manually
-                transform into original data format by utilizing ``CTGANDataTransformer`` instance's
-                ``reverse_transform`` method.
-
-            batch_size: ``int``, default None.
-                If a value is passed, samples will be generated in batches
-                where ``batch_size`` determines the size of each batch.
-                If ``None``, all ``num_samples`` will be generated at once.
-                Note that, if the number of samples to generate aren't huge
-                or you know your hardware can handle to generate all samples at once,
-                you can leave the value to None, otherwise it is recommended to specify
-                a value for batch size.
-        """
-        if batch_size is None:
-            batch_size = num_samples
-        num_steps = num_samples // batch_size
-        num_steps += 1 if num_samples % batch_size != 0 else 0
-        generated_samples = []
-        for _ in tqdm(range(num_steps), desc="Generating Data"):
-            z = tf.random.normal(shape=[batch_size, self.latent_dim])
-            cond_vector = data_sampler.sample_cond_vectors_for_generation(batch_size)
-            input_gen = tf.concat([z, cond_vector], axis=1)
-            generated_samples.append(self.generator(input_gen, training=False))
-        generated_samples = tf.concat(generated_samples, axis=0)
-        generated_samples = generated_samples[:num_samples]
-
-        if reverse_transform:
-            if data_transformer is None:
-                raise ValueError("""To reverse transform the raw generated data, ``data_transformer`` must not be None.
-                         Please pass the instance of ``CTGANDataTransformer`` class that was used to transform the
-                         input data. Or alternatively, you can set ``reverse_transform`` to False, and later
-                         manually reverse transform the generated raw data to original format by utilizing the 
-                         ``CTGANDataTransformer`` instance's ``reverse_transform`` method.""")
-            generated_samples = data_transformer.reverse_transform(x_generated=generated_samples)
-
-        return generated_samples
-
     def get_config(self):
-        config = super().get_config()
-        config.update({'input_dim': self.input_dim,
-                       'metadata': self.metadata,
-                       'generator_units_values': self.generator_units_values,
-                       'discriminator_units_values': self.discriminator_units_values,
-                       'num_discriminator_steps': self.num_discriminator_steps,
-                       'latent_dim': self.latent_dim,
-                       'packing_degree': self.packing_degree,
-                       'gradient_penalty_lambda': self.gradient_penalty_lambda,
-                       }
-                      )
+        config = {'name': self.name,
+                  'trainable': self.trainable,
+                  'input_dim': self.input_dim,
+                  'metadata': self.metadata,
+                  'generator_units_values': self.generator_units_values,
+                  'discriminator_units_values': self.discriminator_units_values,
+                  'num_discriminator_steps': self.num_discriminator_steps,
+                  'latent_dim': self.latent_dim,
+                  'packing_degree': self.packing_degree,
+                  'gradient_penalty_lambda': self.gradient_penalty_lambda,
+                  }
         return config
 
     @classmethod
