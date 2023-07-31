4bbc6e055a5db37f2642767222bb4dabde3b580b
abaid <khawaja.abaid@gmail.com>
2023-07-31 23:31:54 +0500

    Rework unit tests for LayerFlow version of `DNFNet` model

--- teras/layerflow/models/dnfnet_test.py
+++ teras/layerflow/models/dnfnet_test.py
@@ -1,14 +1,31 @@
-from teras.layerflow.models.dnfnet import DNFNetClassifier, DNFNetRegressor
 import tensorflow as tf
+from tensorflow import keras
+from teras.layerflow.models.dnfnet import DNFNet
+from teras.layers.dnfnet import DNNF
+import os
 
 
-def test_rtdl_resnet_classifier_works():
-    data = tf.ones(shape=(16, 10))
-    dnfnet_classifier = DNFNetClassifier(num_formulas=16)
-    dnfnet_classifier(data)
+class NODETest(tf.test.TestCase):
+    def setUp(self):
+        self.inputs = tf.ones((8, 5))
+        self.input_dim = 5
+        self.dnnf_layers = [DNNF(num_formulas=8) for _ in range(3)]
+        self.head = keras.layers.Dense(1)
 
+    def test_valid_call(self):
+        model = DNFNet(input_dim=self.input_dim,
+                       dnnf_layers=self.dnnf_layers,
+                       head=self.head)
+        model(self.inputs)
 
-def test_rtdl_resnet_regressor_works():
-    data = tf.ones(shape=(16, 10))
-    dnfnet_regressor = DNFNetRegressor(num_formulas=16)
-    dnfnet_regressor(data)
+    def test_save_and_load(self):
+        model = DNFNet(input_dim=self.input_dim,
+                       dnnf_layers=self.dnnf_layers,
+                       head=self.head)
+        save_path = os.path.join(self.get_temp_dir(), "dnfnet_lf.keras")
+        model.save(save_path, save_format="keras_v3")
+        reloaded_model = keras.models.load_model(save_path)
+        outputs_original = model(self.inputs)
+        outputs_reloaded = reloaded_model(self.inputs)
+        self.assertAllClose(outputs_original, outputs_reloaded)
+        self.assertAllClose(model.weights, reloaded_model.weights)
