e2b34720fc32c30787c32e6ac86e465b6de4a525
abaid <khawaja.abaid@gmail.com>
2023-11-30 18:43:52 +0500

    Move to Keras 3.0

--- teras/layers/tabtransformer/tabtransformer_column_embedding_test.py
+++ teras/layers/tabtransformer/tabtransformer_column_embedding_test.py
@@ -1,14 +1,14 @@
-import tensorflow as tf
+from keras import ops
 from teras.layers.tabtransformer.tabtransformer_column_embedding import TabTransformerColumnEmbedding
 
 
 def test_tabtransformer_column_embedding_output_shape():
-    inputs = tf.ones((16, 8, 32))
+    inputs = ops.ones((16, 8, 32))
     ce = TabTransformerColumnEmbedding(num_categorical_features=8,
                                        embedding_dim=32)
     outputs = ce(inputs)
-    assert len(tf.shape(outputs)) == 3
-    assert tf.shape(outputs)[0] == 16
-    assert tf.shape(outputs)[1] == 8
-    assert tf.shape(outputs)[2] == 32
+    assert len(ops.shape(outputs)) == 3
+    assert ops.shape(outputs)[0] == 16
+    assert ops.shape(outputs)[1] == 8
+    assert ops.shape(outputs)[2] == 32
 
