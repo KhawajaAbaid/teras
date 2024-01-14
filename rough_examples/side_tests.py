be0e08faaf3827a7d208b66bd62f245a02df0a62
abaid <khawaja.abaid@gmail.com>
2024-01-14 17:02:51 +0500

    Remove rough examples directory

--- rough_examples/side_tests.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import pandas as pd
-from teras.utils import get_features_metadata_for_embedding
-
-data = {'length': tf.ones(shape=(10,)),
-        'area': tf.ones(shape=(10,))}
-inputs_array = tf.transpose(list(data.values()))
-print(inputs_array)
