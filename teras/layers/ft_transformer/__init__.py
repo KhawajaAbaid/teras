7632ee7879c9d01d91ba83c68e4a2c6b6f390546
abaid <khawaja.abaid@gmail.com>
2024-01-29 21:27:05 +0500

    No more imports here

--- teras/layers/ft_transformer/__init__.py
+++ teras/layers/ft_transformer/__init__.py
@@ -11,7 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-from teras.layers.ft_transformer.ft_numerical_feature_embedding import FTNumericalFeatureEmbedding
-from teras.layers.ft_transformer.ft_cls_token import FTCLSToken
