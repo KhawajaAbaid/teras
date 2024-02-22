792e47d4b8a91468f87ac0830988e4d2cb9716e5
abaid <khawaja.abaid@gmail.com>
2024-02-22 16:33:41 +0500

    Update __init__

--- teras/layers/tabnet/__init__.py
+++ teras/layers/tabnet/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Khawaja Abaid Ullah
+# Copyright 2024 Khawaja Abaid Ullah
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,10 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-from teras.layers.tabnet.tabnet_attentive_transformer import TabNetAttentiveTransformer
-from teras.layers.tabnet.tabnet_feature_transformer_block import TabNetFeatureTransformerBlock
-from teras.layers.tabnet.tabnet_feature_transformer import TabNetFeatureTransformer
-from teras.layers.tabnet.tabnet_encoder import TabNetAttentiveTransformer
-from teras.layers.tabnet.tabnet_decoder import TabNetDecoder
