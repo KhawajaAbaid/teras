414c6d0bfe6181470391428f2398d73b589847bd
abaid <khawaja.abaid@gmail.com>
2024-02-28 19:11:01 +0500

    Update `tabnet_reconstruction_loss`

--- teras/losses/tabnet.py
+++ teras/losses/tabnet.py
@@ -1,29 +1,30 @@
-import tensorflow as tf
+from keras import ops
+from keras.backend import floatx
 
 
-def reconstruction_loss(real_samples=None,
-                        reconstructed_samples=None,
-                        mask=None):
+def tabnet_reconstruction_loss(real=None,
+                               reconstructed=None,
+                               mask=None):
     """
-    Reconstruction loss for TabNet Pretrainer mode as propsoed by
-     Sercan et al. in the paper,
-     "TabNet: Attentive Interpretable Tabular Learning"
+    Reconstruction loss for TabNet Pretrainer mode as proposed by
+    Sercan et al. in the paper,
+    "TabNet: Attentive Interpretable Tabular Learning"
 
      Reference(s):
         https://arxiv.org/abs/1908.07442
 
     Args:
-        real_samples: Samples drawn from the input dataset
-        reconstructed_samples: Samples reconstructed by the decoder
-        mask: Mask that indicates the missing of features in a sample
+        real: Samples drawn from the input dataset
+        reconstructed: Samples reconstructed by the decoder
+        mask: Mask that indicates the missing-ness of features in a sample
 
     Returns:
         Reconstruction loss for TabNet Pretraining.
     """
-    nominator_part = (reconstructed_samples - real_samples) * mask
-    real_samples_population_std = tf.math.reduce_std(tf.cast(real_samples, dtype=tf.float32))
+    nominator_part = (reconstructed - real) * mask
+    real_samples_population_std = ops.std(ops.cast(real, dtype=floatx()))
     # divide
     x = nominator_part / real_samples_population_std
     # Calculate L2 norm
-    loss = tf.sqrt(tf.reduce_sum(tf.square(x)))
+    loss = ops.sqrt(ops.sum(ops.square(x)))
     return loss
