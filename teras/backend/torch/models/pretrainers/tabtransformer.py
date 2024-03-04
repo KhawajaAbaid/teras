477306f1f8609ad12a58143ed2cbd80d89eaaaf1
abaid <khawaja.abaid@gmail.com>
2024-03-04 19:02:35 +0500

    Implement `TabTransformerMLMPretrainer` and `TabTransformerRTDPretrainer`

---
+++ teras/backend/torch/models/pretrainers/tabtransformer.py
@@ -0,0 +1,75 @@
+import tensorflow as torch
+import keras
+from teras.backend.common.models.pretrainers.tabtransformer import (
+    BaseTabTransformerMLMPretrainer,
+    BaseTabTransformerRTDPretrainer
+)
+from keras import ops
+
+
+class TabTransformerMLMPretrainer(BaseTabTransformerMLMPretrainer):
+    def __init__(self,
+                 model: keras.Model,
+                 data_dim: int,
+                 k: float = 0.3,
+                 mask_seed: int = 1337,
+                 **kwargs):
+        super().__init__(model=model,
+                         data_dim=data_dim,
+                         k=k,
+                         mask_seed=mask_seed,
+                         **kwargs)
+
+    def train_step(self, data):
+        mask = self._create_mask(ops.shape(data))
+        # In mask, 1 indicates that the feature will be missing, while 0
+        # indicates the opposite
+        y_pred = self(data, mask=(1 - mask))
+        loss = self.compute_loss(y_true=data * mask,
+                                 y_pred=y_pred)
+        # Run backwards pass
+        loss.backward()
+        # Get grads
+        gradients = [v.value.grad for v in self.trainable_variables]
+        # Optimize
+        self.optimizer.apply(
+            gradients,
+            self.trainable_variables
+        )
+
+        logs = {m.name: m.result() for m in self.metrics}
+        return logs
+
+
+class TabTransformerRTDPretrainer(BaseTabTransformerRTDPretrainer):
+    def __init__(self,
+                 model: keras.Model,
+                 data_dim: int,
+                 k: float = 0.3,
+                 mask_seed: int = 1337,
+                 **kwargs):
+        super().__init__(model=model,
+                         data_dim=data_dim,
+                         k=k,
+                         mask_seed=mask_seed,
+                         **kwargs)
+
+    def train_step(self, data):
+        mask = self._create_mask(ops.shape(data))
+        # In mask, 1 indicates that the feature will be missing, while 0
+        # indicates the opposite
+        y_pred = self(data, mask=(1 - mask))
+        loss = self.compute_loss(y_true=mask,
+                                 y_pred=y_pred)
+        # Run backward pass
+        loss.backward()
+        # Get grads
+        gradients = [v.value.grad for v in self.trainable_variables]
+        # Optimize
+        self.optimizer.apply(
+            gradients,
+            self.trainable_variables
+        )
+
+        logs = {m.name: m.result() for m in self.metrics}
+        return logs
