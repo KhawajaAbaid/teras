cd67b245c434406514bd74beaebf6abc0f3765b4
abaid <khawaja.abaid@gmail.com>
2024-03-08 19:25:35 +0500

    various fixes

--- teras/backend/jax/models/pretrainers/tab_transformer.py
+++ teras/backend/jax/models/pretrainers/tab_transformer.py
@@ -1,4 +1,5 @@
 import jax
+import jax.numpy as jnp
 import keras
 from teras.backend.common.models.pretrainers.tab_transformer import (
     BaseTabTransformerMLMPretrainer,
@@ -20,6 +21,21 @@ class TabTransformerMLMPretrainer(BaseTabTransformerMLMPretrainer):
                          mask_seed=mask_seed,
                          **kwargs)
 
+    def _create_mask(self, input_shape):
+        if len(input_shape) != 2:
+            raise ValueError(
+                "Expected `input_shape` to have a rank of 2. "
+                f"Received, {input_shape} with rank {len(input_shape)}")
+        batch_size, _ = input_shape
+        key = jax.random.PRNGKey(self.mask_seed)
+        num_features_to_miss = int(self.missing_rate * self.data_dim)
+        mask = jnp.ones((num_features_to_miss,), dtype=jnp.int32)
+        mask = jnp.pad(mask,
+                       (0, self.data_dim - num_features_to_miss))
+        mask = jnp.tile(jnp.expand_dims(mask, 0), (batch_size, 1))
+        mask = jax.random.permutation(key, mask, axis=1, independent=True)
+        return mask
+
     def compute_loss_and_updates(self,
                                  trainable_variables,
                                  non_trainable_variables,
@@ -33,7 +49,7 @@ class TabTransformerMLMPretrainer(BaseTabTransformerMLMPretrainer):
             mask=(1 - mask),
             training=training
         )
-        loss = self.compute_loss(y_true=x * mask,
+        loss = self.compute_loss(y=x * mask,
                                  y_pred=y_pred)
         return loss, (y_pred, non_trainable_variables)
 
@@ -111,6 +127,21 @@ class TabTransformerRTDPretrainer(BaseTabTransformerRTDPretrainer):
                          mask_seed=mask_seed,
                          **kwargs)
 
+    def _create_mask(self, input_shape):
+        if len(input_shape) != 2:
+            raise ValueError(
+                "Expected `input_shape` to have a rank of 2. "
+                f"Received, {input_shape} with rank {len(input_shape)}")
+        batch_size, _ = input_shape
+        key = jax.random.PRNGKey(self.mask_seed)
+        num_features_to_replace = int(self.replace_rate * self.data_dim)
+        mask = jnp.ones((num_features_to_replace,), dtype=jnp.int32)
+        mask = jnp.pad(mask,
+                       (0, self.data_dim - num_features_to_replace))
+        mask = jnp.tile(jnp.expand_dims(mask, 0), (batch_size, 1))
+        mask = jax.random.permutation(key, mask, axis=1, independent=True)
+        return mask
+
     def compute_loss_and_updates(self,
                                  trainable_variables,
                                  non_trainable_variables,
@@ -124,7 +155,7 @@ class TabTransformerRTDPretrainer(BaseTabTransformerRTDPretrainer):
             mask=(1 - mask),
             training=training
         )
-        loss = self.compute_loss(y_true=mask,
+        loss = self.compute_loss(y=mask,
                                  y_pred=y_pred)
         return loss, (y_pred, non_trainable_variables)
 
