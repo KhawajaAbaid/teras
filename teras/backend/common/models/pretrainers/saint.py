dc71776f7c1abd6c90e3b6852439a12ba6c05d74
abaid <khawaja.abaid@gmail.com>
2024-03-13 08:04:08 +0500

    Implement `compute_loss`, `reset_metrics` and other relevant updates

--- teras/backend/common/models/pretrainers/saint.py
+++ teras/backend/common/models/pretrainers/saint.py
@@ -5,8 +5,7 @@ from teras.layers.mixup import MixUp
 from teras.layers.saint.embedding import SAINTEmbedding
 from teras.layers.saint.projection_head import SAINTProjectionHead
 from teras.layers.saint.reconstruction_head import SAINTReconstructionHead
-from teras.losses.saint import (saint_constrastive_loss,
-                                saint_denoising_loss)
+import numpy as np
 
 
 class BaseSAINTPretrainer(keras.Model):
@@ -66,36 +65,122 @@ class BaseSAINTPretrainer(keras.Model):
             name="reconstruction_head"
         )
 
-        self._pretrained = False
-        self._constrastive_loss_tracker = keras.metrics.Mean(
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+        self.constrastive_loss_tracker = keras.metrics.Mean(
             name="constrastive_loss"
         )
-        self._denoising_loss_tracker = keras.metrics.Mean(
+        self.denoising_loss_tracker = keras.metrics.Mean(
             name="denoising_loss"
         )
 
+        self._pretrained = False
+
     def build(self, input_shape):
+        self.cutmix.build(input_shape)
+        self.mixup.build(input_shape)
         self.embedding.build(input_shape)
         input_shape = self.embedding.compute_output_shape(input_shape)
         self.model.build(input_shape)
         input_shape = self.model.compute_output_shape(input_shape)
         self.projection_head_real.build(input_shape)
         self.projection_head_mixed.build(input_shape)
+        self.flatten.build(input_shape)
         self.reconstruction_head.build(input_shape)
 
-    def compile(self,
-                contrastive_loss=saint_constrastive_loss,
-                denoising_loss=saint_denoising_loss,
-                **kwargs):
-        super().compile(**kwargs)
-        self.contrastive_loss = contrastive_loss
-        self.denoising_loss = denoising_loss
+    def compute_loss(self, x, x_reconstructed, z, z_augmented, cardinalities,
+                     temperature=0.7, lambda_=10., lambda_c=0.5):
+        """
+
+        Args:
+            x: Samples drawn from the original dataset.
+            x_reconstructed: Samples reconstructed by the reconstruction head.
+            z: Encodings of the real samples
+            z_augmented: Encodings of the augmented samples
+            cardinalities: list, a list cardinalities of all the features
+                in the dataset in the same order as the features' occurrence.
+                For numerical features, use the value `0` as indicator at
+                the corresponding index.
+                You can use the `compute_cardinalities` function from
+                `teras.utils` package for this purpose.
+            temperature: float, used in the computation of the
+                `contrastive_loss` to scale logits. Defaults to 0.7
+            lambda_: float, acts as a weight when adding the contrastive loss
+                and the denoising loss together.
+                `loss = constrastive_loss + lambda_ * denoising_loss`
+                Defaults to 10.
+            lambda_c: float, used in the computation of the contrastive
+                loss. Similar to `lambda_` is helps combined two sub-losses
+                within the contrastive loss. Defaults to 0.5
+
+        Returns:
+            Combined contrastive and denoising loss.
+        """
+        # ====================
+        # Constrastive loss
+        # ====================
+        batch_size = ops.shape(z)[0]
+        labels = ops.arange(batch_size)
+        logits_ab = ops.matmul(z, ops.transpose(z_augmented)) / temperature
+        logits_ba = ops.matmul(z_augmented, ops.transpose(z)) / temperature
+        loss_a = keras.losses.SparseCategoricalCrossentropy(from_logits=True)(
+            y_true=labels,
+            y_pred=logits_ab)
+        loss_b = keras.losses.SparseCategoricalCrossentropy(from_logits=True)(
+            y_true=labels,
+            y_pred=logits_ba)
+        c_loss = lambda_c * (loss_a + loss_b) / 2
+
+        # ==================
+        # Denoising loss
+        # ==================
+        num_features = len(cardinalities)
+        categorical_features_idx = np.flatnonzero(cardinalities)
+        num_categorical_features = len(categorical_features_idx)
+        categorical_cardinalities = np.asarray(cardinalities)[
+            categorical_features_idx]
+        total_categories = sum(cardinalities)
+        d_loss = 0.
+        # The reconstructed samples have dimensions equal to number of
+        # categorical features + number of categories in all the categorical
+        # features combined.
+        # In other words, each categorical feature gets expanded into
+        # `number of categories` features due to softmax layer during the
+        # reconstruction phase.
+        # Since the order they occur in is still the same
+        # except for that the first `total_categories` number of features are
+        # categorical and following are continuous
+        for current_idx, feature_idx in enumerate(categorical_features_idx):
+            num_categories = categorical_cardinalities[current_idx]
+            d_loss += keras.losses.SparseCategoricalCrossentropy(
+                from_logits=True)(
+                # real_samples have the original feature order
+                y_true=x[:, feature_idx],
+                y_pred=x_reconstructed[:, current_idx: current_idx + num_categories]
+            )
+            current_idx += 1
+        # Check if there are numerical features -- if so, compute the mse loss
+        mse_loss = ops.cond(pred=num_categorical_features < num_features,
+                            true_fn=lambda: ops.sum(
+                                keras.losses.MeanSquaredError()(
+                                    x[:, num_categorical_features:],
+                                    x_reconstructed[:, total_categories:])),
+                            false_fn=lambda: 0.
+                            )
+        d_loss += mse_loss
+        loss = c_loss + lambda_ * d_loss
+        return loss, c_loss, d_loss
+
+    def reset_metrics(self):
+        self.loss_tracker.reset_state()
+        self.constrastive_loss_tracker.reset_state()
+        self.denoising_loss_tracker.reset_state()
 
     @property
     def metrics(self):
-        metrics = super().metrics()
-        metrics.extend([self._constrastive_loss_tracker,
-                        self._denoising_loss_tracker])
+        metrics = [self.loss_tracker,
+                   self.constrastive_loss_tracker,
+                   self.denoising_loss_tracker,
+                   ]
         return metrics
 
     def call(self, inputs, **kwargs):
