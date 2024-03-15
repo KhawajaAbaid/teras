5ee3db4976f24b812201d92619da2ab4f5c1374d
abaid <khawaja.abaid@gmail.com>
2024-03-15 21:29:10 +0500

    Update `BaseDataTransformer` class

--- teras/preprocessing/base/base_data_transformer.py
+++ teras/preprocessing/base/base_data_transformer.py
@@ -4,32 +4,48 @@ import pandas as pd
 
 class BaseDataTransformer:
     """
-    Base class for ``DataTransformer`` classes.
+    Base `DataTransformer` class.
     It provides the common methods and attributes.
     """
     def __init__(self):
-        self.metadata = dict()
-        self.metadata["numerical"] = dict()
-        self.metadata["categorical"] = dict()
-        self.fitted = False
+        self._metadata = dict()
+        self._metadata["continuous"] = dict()
+        self._metadata["categorical"] = dict()
+        self._fitted = False
 
     @abstractmethod
     def fit(self, x, **kwargs):
-        pass
+        raise NotImplementedError
 
     @abstractmethod
-    def transform(self, x, **kwargs):
-        pass
+    def transform(self, x):
+        raise NotImplementedError
 
     def get_metadata(self):
         """
         Returns:
             named tuple of features metadata.
         """
-        return self.metadata
+        if not self._fitted:
+            raise AssertionError(
+                "The `fit` method has not yet been called. Please fit the "
+                "`DataTransformer` before accessing the `get_metadata` method."
+            )
+        return self._metadata
 
     def fit_transform(self,
-                      x: pd.DataFrame,
-                      return_dataframe: bool = True):
+                      x: pd.DataFrame):
         self.fit(x)
-        return self.transform(x, return_dataframe=return_dataframe)
+        return self.transform(x)
+
+    @abstractmethod
+    def get_config(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def save(self, filename):
+        raise NotImplementedError
+
+    @abstractmethod
+    def load(self, filename):
+        raise NotImplementedError
