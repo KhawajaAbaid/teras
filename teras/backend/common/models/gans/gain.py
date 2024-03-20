b0535d92a544ca635de26f96515c97d92a702c18
abaid <khawaja.abaid@gmail.com>
2024-03-20 16:48:08 +0500

    Fix args spacing

--- teras/backend/common/models/gans/gain.py
+++ teras/backend/common/models/gans/gain.py
@@ -131,18 +131,9 @@ class BaseGAIN(keras.Model):
         imputed_data = mask * data + (1 - mask) * imputed_data
         return imputed_data
 
-    def impute(self,
-               x,
-               data_transformer=None,
-               reverse_transform=True,
-               batch_size=None,
-               verbose="auto",
-               steps=None,
-               callbacks=None,
-               max_queue_size=10,
-               workers=1,
-               use_multiprocessing=False
-               ):
+    def impute(self, x, data_transformer=None, reverse_transform=True,
+               batch_size=None, verbose="auto", steps=None, callbacks=None,
+               max_queue_size=10, workers=1, use_multiprocessing=False):
         """
         Impute function combines GAIN's `predict` method and
         DataTransformer's `reverse_transform` method to fill
