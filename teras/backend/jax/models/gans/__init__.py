60af5c05493d40ed3d83cab333692c66611dfc7f
abaid <khawaja.abaid@gmail.com>
2024-03-29 19:26:49 +0500

    license

--- teras/backend/jax/models/gans/__init__.py
+++ teras/backend/jax/models/gans/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Khawaja Abaid Ullah
+# Copyright 2024 Khawaja Abaid Ullah
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,14 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
-# General purpose common layers
-from teras.layerflow.layers.common.common import HiLOL
-
-
-# Transformer layers
-from teras.layerflow.layers.common.transformer import (FeedForward,
-                                                       Transformer,
-                                                       Encoder)
-
