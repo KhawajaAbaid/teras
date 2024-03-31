2ff9ce0ce0808aeed778c6380ffd6aa9a8cf98eb
abaid <khawaja.abaid@gmail.com>
2024-03-31 16:58:06 +0500

    Implement `CTGANGeneratorLayer` anew

--- teras/layers/ctgan/generator_layer.py
+++ teras/layers/ctgan/generator_layer.py
@@ -1,30 +1,36 @@
 import keras
+from teras.api_export import teras_export
 
 
-@keras.saving.register_keras_serializable(package="teras.layers.ctgan")
-class CTGANGeneratorBlock(keras.layers.Layer):
+@teras_export("teras.layers.CTGANGeneratorLayer")
+class CTGANGeneratorLayer(keras.layers.Layer):
     """
     Residual Block for Generator as used by the authors of CTGAN
     proposed in the paper Modeling Tabular data using Conditional GAN.
 
-    ``outputs = Concat([ReLU(BatchNorm(Dense(inputs))), inputs])``
+    `outputs = Concat([ReLU(BatchNorm(Dense(inputs))), inputs])`
 
     Reference(s):
         https://arxiv.org/abs/1907.00503
 
     Args:
-        units: ``int``, default 256,
-            Dimensionality of the hidden layer
+        dim: int, Dimensionality of the hidden layer.
+            Defaults to 256.
     """
     def __init__(self,
-                 units: int = 256,
+                 dim: int = 256,
                  **kwargs):
         super().__init__(**kwargs)
-        self.units = units
-        self.dense = keras.layers.Dense(self.units)
+        self.dim = dim
+        self.dense = keras.layers.Dense(self.dim)
         self.batch_norm = keras.layers.BatchNormalization()
         self.relu = keras.layers.ReLU()
         self.concat = keras.layers.Concatenate(axis=1)
+
+    def build(self, input_shape):
+        self.dense.build(input_shape)
+        input_shape = self.dense.compute_output_shape(input_shape)
+        self.batch_norm.build(input_shape)
 
     def call(self, inputs):
         x = self.dense(inputs)
@@ -33,7 +39,10 @@ class CTGANGeneratorBlock(keras.layers.Layer):
         out = self.concat([x, inputs])
         return out
 
+    def compute_output_shape(self, input_shape):
+        return input_shape[:-1] + (input_shape[-1] + self.dim,)
+
     def get_config(self):
         config = super().get_config()
-        config.update({"units": self.units})
+        config.update({"dim": self.dim})
         return config
