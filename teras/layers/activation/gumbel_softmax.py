183163a910f98f48072ba58b93b92d128b604a53
abaid <khawaja.abaid@gmail.com>
2024-04-01 22:07:16 +0500

    Seed gumbel softmax

--- teras/layers/activation/gumbel_softmax.py
+++ teras/layers/activation/gumbel_softmax.py
@@ -21,19 +21,23 @@ class GumbelSoftmax(keras.layers.Layer):
             concentrated around the category with the highest probability.
         hard: bool, Whether to return soft probabilities or hard one hot
         vectors. Defaults to False.
+        seed: int, Seed for random sampling.
     """
     def __init__(self,
                  temperature: float = 0.2,
                  hard: bool = False,
+                 seed: int = 1337,
                  **kwargs):
         super().__init__(**kwargs)
         self.temperature = temperature
         self.hard = hard
+        self.seed = seed
 
     def call(self, logits):
         return gumbel_softmax(logits,
                               temperature=self.temperature,
-                              hard=self.hard)
+                              hard=self.hard,
+                              seed=self.seed)
 
     def get_config(self):
         config = super().get_config()
