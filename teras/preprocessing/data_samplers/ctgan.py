0e2ed091ce3519f61af7bb08c98a9d361ea6a28d
abaid <khawaja.abaid@gmail.com>
2024-04-02 23:32:04 +0500

    A complete rework - yes once again ikr

--- teras/preprocessing/data_samplers/ctgan.py
+++ teras/preprocessing/data_samplers/ctgan.py
@@ -1,5 +1,3 @@
-from keras import ops
-from keras import random
 from teras.utils.types import FeaturesNamesType
 import numpy as np
 try:
@@ -30,23 +28,42 @@ class CTGANDataSampler:
             if your dataset doesn't contain any categorical features,
             consider using some other generative model.
         continuous_features: list, List of continuous features names
-        batch_size: ``int``, default 512,
+        batch_size: int, default 512,
             Batch size to use for the dataset.
+        seed: int, Seed for random ops.
     """
     def __init__(self,
                  metadata,
                  categorical_features: FeaturesNamesType,
                  continuous_features: FeaturesNamesType = None,
-                 batch_size: int = 512):
+                 batch_size: int = 512,
+                 seed: int = 1337):
         self.metadata = metadata
         self.batch_size = batch_size
         self.categorical_features = categorical_features
         self.continuous_features = continuous_features
+        self.seed = seed
 
+        self._np_rng = np.random.default_rng(self.seed)
         self.num_samples = None
         self.data_dim = None
         self.batch_size = batch_size
         self.row_idx_by_categories = list()
+        self._num_categorical_features = len(self.categorical_features)
+        self._cat_features_relative_idx = np.asarray(
+            self.metadata["categorical"]["relative_indices_all"])
+        self._total_categories = self.metadata["categorical"]["total_num_categories"]
+        self._all_categories = self.metadata["categorical"]["categories_all"]
+        self._categories_probs_all = self.metadata["categorical"]["categories_probs_all"]
+
+        # Since the nested lists have different lengths, so lets pad
+        max_num_categories = max([len(categories)
+                                  for categories in self._all_categories])
+        self._features_categories_probs = np.array([
+            np.pad(probs, (0, max_num_categories - len(probs)),
+                   constant_values=0.)
+            for probs in self._categories_probs_all
+        ])
 
     def get_dataset(self,
                     x_transformed,
@@ -76,68 +93,65 @@ class CTGANDataSampler:
 
         dataset = tf.data.Dataset.from_generator(
             self.generator,
-            output_signature=(tf.TensorSpec(
-                            shape=(self.batch_size, tf.shape(x_transformed)[1]),
-                            name="real_samples"),
-                              tf.TensorSpec(
-                                  shape=(self.batch_size, total_num_categories),
-                                  dtype=tf.float32, name="cond_vectors_real"),
-                              tf.TensorSpec(
-                                  shape=(self.batch_size, total_num_categories),
-                                  dtype=tf.float32, name="cond_vectors"),
-                              tf.TensorSpec(
-                                  shape=(self.batch_size, len(self.categorical_features)),
-                                  dtype=tf.float32, name="mask")),
-            args=[x_transformed]
+            output_signature=(
+                tf.TensorSpec(
+                    shape=(None, self.data_dim),
+                    name="real_samples"),
+                tf.TensorSpec(
+                    shape=(None, total_num_categories),
+                    dtype=tf.float32, name="cond_vectors_real"),
+                tf.TensorSpec(
+                    shape=(None, total_num_categories,),
+                    dtype=tf.float32, name="cond_vectors"),
+                tf.TensorSpec(
+                    shape=(None, self._num_categorical_features,),
+                    dtype=tf.float32, name="mask")
+            ),
+            args=(x_transformed,)
         )
         return dataset
 
-    def sample_cond_vectors_for_training(self,
-                                         random_features_idx=None,
-                                         random_values_idx=None):
+    def sample_cond_vectors_for_training(self, batch_size):
         # 1. Create Nd zero-filled mask vectors mi = [mi(k)] where k=1...|Di|
         # and for i = 1,...,Nd, so the ith mask vector corresponds to the ith
         # column, and each component is associated to the category of that
         # column.
-        masks = ops.zeros([self.batch_size, len(self.categorical_features)])
-        cond_vectors = ops.zeros(
-            [self.batch_size, self.metadata["categorical"]["total_num_categories"]])
+        mask = np.zeros((batch_size, self._num_categorical_features))
+        cond_vectors = np.zeros((batch_size, self._total_categories))
         # 2. Randomly select a discrete column Di out of all the Nd discrete
         # columns, with equal probability.
-        # >>> We select them in generator method
-
-        random_features_relative_indices = ops.take(
-            self.metadata["categorical"]["relative_indices_all"],
-            indices=random_features_idx)
+        selected_cat_features_idx = self._np_rng.choice(
+            np.arange(self._num_categorical_features),
+            size=batch_size
+        )
+        selected_cat_features_relative_idx = self._cat_features_relative_idx[
+            selected_cat_features_idx]
 
         # 3. Construct a PMF across the range of values of the column selected
         # in 2, Di* , such that the probability mass of each value is the
         # logarithm of its frequency in that column.
-        # >>> Moved to the generator method, which calls this method
-        # and passes the value of `random_features_idx`
 
-        # Choose random value index for each feature
-        # >>> Moved to the generator method, which calls this method
-        # and passes the value of `random_values_idx`
+        # NOTE: We've precomputed the probabilities in the DataTransformer
+        # class for each feature already to speed things up.
+        selected_features_categories_probs = self._features_categories_probs[
+            selected_cat_features_idx]
+
+        # Choose random values idx for features
+        selected_cat_values_idx = np.array([
+            self._np_rng.choice(np.arange(len(probs)),
+                                p=probs)
+            for probs in selected_features_categories_probs]
+        ).astype(np.int32)
 
         # Offset this index by relative index of the feature that it belongs to.
         # because the final cond vector is the concatenation of all features and
         # is just one vector that has the length equal to total_num_categories
-        random_values_idx_offsetted = random_values_idx + ops.cast(
-            random_features_relative_indices, dtype="int32")
-        # Indices are required by the tensor_scatter_nd_update method to be of
-        # type int32 and NOT int64 random_values_idx_offsetted = tf.cast(
-        # random_values_idx_offsetted, dtype=tf.int32)
-        # indices_cond = list(zip(tf.range(batch_size), random_values_idx_offsetted))
-        indices_cond = ops.stack([ops.arange(self.batch_size), random_values_idx_offsetted], axis=1)
-        ones = ops.ones(self.batch_size)
-        cond_vectors = ops.scatter_update(cond_vectors, indices_cond, ones)
-        # indices_mask = list(zip(tf.range(batch_size), tf.cast(random_features_idx, dtype=tf.int32)))
-        indices_mask = ops.stack([ops.arange(self.batch_size),
-                                  random_features_idx], axis=1)
-        masks = ops.scatter_update(masks, indices_mask,
-                                   ops.ones(self.batch_size))
-        return cond_vectors, masks
+        selected_cat_values_idx_offsetted = selected_cat_features_relative_idx
+
+        cond_vectors[np.arange(batch_size), selected_cat_values_idx_offsetted] = 1
+        mask[np.arange(batch_size), selected_cat_features_idx] = 1
+        return (cond_vectors, mask, selected_cat_features_idx,
+                selected_cat_values_idx)
 
     def sample_cond_vectors_for_generation(self, batch_size):
         """
@@ -145,44 +159,33 @@ class CTGANDataSampler:
         we sample indices purely randomly instead of based on the calculated
         probability as proposed in the paper.
         """
-        num_categories_all = self.metadata["categorical"]["num_categories_all"]
-        cond_vectors = ops.zeros(
-            (batch_size, self.metadata["categorical"]["total_num_categories"]))
-        random_features_idx = ops.cast(
-            random.uniform(shape=(batch_size,),
-                           minval=0,
-                           maxval=len(self.categorical_features)),
-            dtype="int32")
+        num_categories_all = np.array(
+            self.metadata["categorical"]["num_categories_all"])
+        cond_vectors = np.zeros((batch_size, self._total_categories))
+        selected_cat_features_idx = self._np_rng.choice(
+            np.arange(self._num_categorical_features),
+            size=batch_size
+        )
+        selected_cat_features_relative_idx = self._cat_features_relative_idx[
+            selected_cat_features_idx]
 
         # For each randomly picked feature, we get it's corresponding
         # num_categories
-        random_num_categories_all = ops.take(num_categories_all,
-                                             indices=random_features_idx)
+        selected_num_categories_all = num_categories_all[selected_cat_features_idx]
         # Then we select one category index from a feature using a range of
         # 0 — num_categories
-        random_values_idx = [ops.squeeze(ops.cast(
-            random.uniform(shape=(1,),
-                           minval=0,
-                           maxval=num_categories),
-            dtype="int32"))
-            for num_categories in random_num_categories_all]
-        random_values_idx = ops.stack(random_values_idx)
+        selected_values_idx = np.array([
+            self._np_rng.choice(np.arange(num_categories))
+            for num_categories in selected_num_categories_all]
+        ).astype(np.int32)
         # Offset this index by relative index of the feature that it belongs to.
         # because the final cond vector is the concatenation of all features and
         # is just one vector that has the length equal to total_num_categories
-        random_features_relative_indices = ops.take(
-            self.metadata["categorical"]["relative_indices_all"],
-            indices=random_features_idx)
-        random_values_idx_offsetted = random_values_idx + ops.cast(
-            random_features_relative_indices, dtype="int32")
-        # random_values_idx_offsetted = tf.cast(random_values_idx_offsetted, dtype=tf.int32)
-        indices_cond = list(zip(ops.arange(batch_size),
-                                random_values_idx_offsetted))
-        ones = ops.ones(batch_size)
-        cond_vectors = ops.scatter_update(cond_vectors, indices_cond, ones)
+        selected_values_idx += selected_cat_features_relative_idx
+        cond_vectors[np.arange(batch_size), selected_values_idx] = 1
         return cond_vectors
 
-    def generator(self, x_transformed, for_tvae=False):
+    def generator(self, x_transformed):
         """
         Used to create a tensorflow dataset.
         Returns:
@@ -197,53 +200,38 @@ class CTGANDataSampler:
         # now let's just work with it.
         num_steps_per_epoch = self.num_samples // self.batch_size
         for _ in range(num_steps_per_epoch):
-            random_features_idx = ops.cast(
-                random.uniform([self.batch_size],
-                               minval=0,
-                               maxval=len(self.categorical_features)),
-                dtype="int32")
-            # NOTE: We've precomputed the probabilities in the DataTransformer
-            # class for each feature already to speed things up.
-            random_features_categories_probs = ops.take(
-                self.metadata["categorical"]["categories_probs_all"],
-                indices=random_features_idx)
-            random_values_idx = [
-                random_choice(feature_probs)
-                for feature_probs in random_features_categories_probs]
-            random_values_idx = ops.cast(ops.squeeze(random_values_idx),
-                                         dtype="int32")
+            # `cond_vectors` will be first concatenated with the noise
+            # vector `z` to create generator input and then will be concatenated
+            # with the generated samples to serve as input for discriminator
+            (
+                cond_vectors, mask, selected_cat_features_idx,
+                selected_cat_values_idx
+            ) = self.sample_cond_vectors_for_training(self.batch_size)
+
             # the official implementation uses actual indices during the
-            # sample_cond_vector method ut uses the shuffled version in
+            # sample_cond_vector method but uses the shuffled version in
             # sampling data, so we're gonna do just that.
-            shuffled_idx = random.shuffle(ops.arange(self.batch_size))
-            # features_idx = random_features_idx
-            # values_idx = random_values_idx
-            shuffled_features_idx = ops.take(random_features_idx,
-                                             indices=shuffled_idx)
-            shuffled_values_idx = ops.take(random_values_idx,
-                                           indices=shuffled_idx)
+            shuffled_idx = np.arange(self.batch_size)
+            self._np_rng.shuffle(shuffled_idx)
+
+            shuffled_cat_features_idx = selected_cat_features_idx[shuffled_idx]
+            shuffled_values_idx = selected_cat_values_idx[shuffled_idx]
+
             sample_idx = []
-            for feat_id, val_id in zip(shuffled_features_idx,
+            for feat_id, val_id in zip(shuffled_cat_features_idx,
                                        shuffled_values_idx):
-                s_id = np.random.choice(
-                    self.row_idx_by_categories[ops.squeeze(feat_id)][ops.squeeze(val_id)])
-                sample_idx.append(ops.squeeze(s_id))
+                s_id = self._np_rng.choice(
+                    self.row_idx_by_categories[np.squeeze(feat_id)][
+                        np.squeeze(val_id)])
+                sample_idx.append(np.squeeze(s_id))
 
             # we also return shuffled_idx because it will be required to shuffle
             # the conditional vector in the training loop as we want to keep
             # the shuffling consistent as the batch of transformed data and
             # cond vector must have one to one feature correspondence.
-            # yield x_transformed[sample_idx], shuffled_idx,
-            # random_features_idx, random_values_idx
 
-            # `cond_vectors` will be first concatenated with the noise
-            # vector `z` to create generator input and then will be concatenated
-            # with the generated samples to serve as input for discriminator
-            cond_vectors, mask = self.sample_cond_vectors_for_training(
-                random_features_idx=random_features_idx,
-                random_values_idx=random_values_idx)
             # `cond_vectors_real` will be concatenated with the real_samples
             # and passed to the discriminator
-            cond_vectors_real = ops.take(cond_vectors, indices=shuffled_idx)
+            cond_vectors_real = cond_vectors[shuffled_idx]
             real_samples = x_transformed[sample_idx]
             yield real_samples, cond_vectors_real, cond_vectors, mask
