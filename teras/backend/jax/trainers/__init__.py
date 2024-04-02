73e4a706100781b841aea6aad154b7a31198ef24
abaid <khawaja.abaid@gmail.com>
2024-04-02 06:55:47 +0500

    creating new software engineering paradigm

--- teras/backend/jax/trainers/__init__.py
+++ teras/backend/jax/trainers/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Good code? Bad code?
+from teras.backend.jax.trainers import gain as GAINTrainer
