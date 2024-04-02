7f248e33cc8d83abcc78ce87d32f0535f1af4287
abaid <khawaja.abaid@gmail.com>
2024-04-02 13:29:52 +0500

    Wait there's a choice

--- teras/backend/jax/models/gans/gain.py
+++ teras/backend/jax/models/gans/gain.py
@@ -15,7 +15,9 @@ class GAIN(JAXGAN):
     raise ImportError(
         "JAX backend doesn't provide a MODEL implementation for `GAIN`. "
         "Rather it offers `GAINTrainer` based on functional approach which "
-        "can be accessed through `teras.trainers` package. "
+        "can be accessed through `teras.trainers` package. \n"
+        "Alternatively, if you really want to use a model based implementation "
+        "for `CTGAN`, you can use the `tensorflow` or `torch` backend."
     )
     def __init__(self,
                  generator: keras.Model,
