74a9aace26469f9c1f90a4b7bcbb895c5ceea1c2
abaid <khawaja.abaid@gmail.com>
2024-04-04 06:59:12 +0500

    istg if i forget to do this all in one single commit again, imma rage

--- teras/backend/torch/models/__init__.py
+++ teras/backend/torch/models/__init__.py
@@ -25,3 +25,6 @@ from teras.backend.torch.models.gans.gain import GAIN
 from teras.backend.torch.models.gans.pcgain import PCGAIN
 from teras.backend.torch.models.gans.ctgan.discriminator import CTGANDiscriminator
 from teras.backend.torch.models.gans.ctgan.ctgan import CTGAN
+
+# Autoencoders
+from teras.backend.torch.models.autoencoders.tvae.tvae import TVAE
