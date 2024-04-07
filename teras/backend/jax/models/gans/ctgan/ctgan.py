14bf7eb1be6de887ac9a083ce6021f9863c0333a
abaid <khawaja.abaid@gmail.com>
2024-04-07 21:57:52 +0500

    Loads of fixes. too many to count. too sleepy to remember. too tired to care.

--- teras/backend/jax/models/gans/ctgan/ctgan.py
+++ teras/backend/jax/models/gans/ctgan/ctgan.py
@@ -24,6 +24,62 @@ class CTGAN(JAXGAN, BaseCTGAN):
                            latent_dim=latent_dim,
                            seed=seed)
 
+    @staticmethod
+    def compute_generator_loss(x_generated,
+                               y_pred_generated,
+                               cond_vectors,
+                               mask,
+                               metadata):
+        """
+        Loss for the Generator model in the CTGAN architecture.
+
+        Args:
+            x_generated: Samples drawn from the input dataset
+            y_pred_generated: Discriminator's output for the generated samples
+            cond_vectors: Conditional vectors that are used for and with
+                generated samples
+            mask: Mask created during the conditional vectors generation step
+            metadata: dict, metadata computed during the data transformation step.
+
+        Returns:
+            Generator's loss.
+        """
+        loss = []
+        cross_entropy_loss = keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True, reduction=None)
+        continuous_features_relative_indices = metadata["continuous"][
+            "relative_indices_all"]
+        features_relative_indices_all = metadata["relative_indices_all"]
+        num_categories_all = metadata["categorical"]["num_categories_all"]
+        # the first k features in the data are continuous which we'll ignore as
+        # we're only concerned with the categorical features here
+        offset = len(continuous_features_relative_indices)
+        for i, index in enumerate(features_relative_indices_all[offset:]):
+            start_idx = index
+            slice_size = num_categories_all[i]
+            logits = jax.lax.dynamic_slice_in_dim(
+                x_generated, start_index=start_idx,
+                slice_size=slice_size, axis=1
+            )
+            start_idx = i
+            slice_size = num_categories_all[i]
+            temp_cond_vector = jax.lax.dynamic_slice_in_dim(
+                cond_vectors, start_index=start_idx,
+                slice_size=slice_size, axis=1
+            )
+            labels = ops.argmax(temp_cond_vector, axis=1)
+            ce_loss = cross_entropy_loss(y_pred=logits,
+                                         y_true=labels
+                                         )
+            loss.append(ce_loss)
+        loss = ops.stack(loss, axis=1)
+        loss = ops.sum(
+            loss * ops.cast(mask, dtype="float32")
+                       ) / ops.cast(ops.shape(y_pred_generated)[0],
+                                    dtype="float32")
+        loss = -ops.mean(y_pred_generated) * loss
+        return loss
+
     def generator_compute_loss_and_updates(
             self,
             generator_trainable_vars,
@@ -35,13 +91,19 @@ class CTGAN(JAXGAN, BaseCTGAN):
             mask,
             metadata
     ):
-        x_generated = self.generator.stateless_call(
+        (
+            x_generated,
+            generator_non_trainable_vars
+        ) = self.generator.stateless_call(
             generator_trainable_vars,
             generator_non_trainable_vars,
             noise)
         x_generated = ops.concatenate(
             [x_generated, cond_vectors], axis=1)
-        y_pred_generated = self.discriminator.stateless_call(
+        (
+            y_pred_generated,
+            discriminator_non_trainable_vars
+        ) = self.discriminator.stateless_call(
             discriminator_trainable_vars,
             discriminator_non_trainable_vars,
             x_generated)
@@ -57,15 +119,27 @@ class CTGAN(JAXGAN, BaseCTGAN):
             x,
             x_generated,
     ):
-        y_pred_generated = self.discriminator.stateless_call(
+        (
+            y_pred_generated,
+            discriminator_non_trainable_vars
+        ) = self.discriminator.stateless_call(
             discriminator_trainable_vars,
             discriminator_non_trainable_vars,
             x_generated)
-        y_pred_real = self.discriminator.stateless_call(
+        (
+            y_pred_real,
+            discriminator_non_trainable_vars
+        ) = self.discriminator.stateless_call(
             discriminator_trainable_vars,
             discriminator_non_trainable_vars,
             x)
-        grad_pen = self.discriminator.gradient_penalty(x, x_generated)
+        (
+            grad_pen,
+            discriminator_non_trainable_vars
+        ) = self.discriminator.gradient_penalty(
+            discriminator_trainable_vars,
+            discriminator_non_trainable_vars,
+            x, x_generated)
         return grad_pen, (discriminator_non_trainable_vars,)
 
     def discriminator_compute_loss_and_updates(
@@ -75,11 +149,17 @@ class CTGAN(JAXGAN, BaseCTGAN):
             x,
             x_generated,
     ):
-        y_pred_generated = self.discriminator.stateless_call(
+        (
+            y_pred_generated,
+            discriminator_non_trainable_vars
+        ) = self.discriminator.stateless_call(
             discriminator_trainable_vars,
             discriminator_non_trainable_vars,
             x_generated)
-        y_pred_real = self.discriminator.stateless_call(
+        (
+            y_pred_real,
+            discriminator_non_trainable_vars
+        ) = self.discriminator.stateless_call(
             discriminator_trainable_vars,
             discriminator_non_trainable_vars,
             x)
@@ -101,7 +181,7 @@ class CTGAN(JAXGAN, BaseCTGAN):
         generator_trainable_vars = trainable_variables[
                                    :len(self.generator.trainable_variables)]
         generator_non_trainable_vars = non_trainable_variables[1:][
-                                   :len(self.generator.non_trainable_variables)]
+                                       :len(self.generator.non_trainable_variables)]
         generator_optimizer_vars = optimizer_variables[
                                    :len(self.generator_optimizer.variables)]
 
@@ -109,7 +189,7 @@ class CTGAN(JAXGAN, BaseCTGAN):
         discriminator_trainable_vars = trainable_variables[
                                        len(self.generator.trainable_variables):]
         discriminator_non_trainable_vars = non_trainable_variables[1:][
-                                   len(self.generator.non_trainable_variables):]
+                                           len(self.generator.non_trainable_variables):]
         discriminator_optimizer_vars = optimizer_variables[
                                        len(self.generator_optimizer.variables):
                                        ]
